2bed63983a79211e5e6880defe6c184fec3b7970
M66B <M66B@users.noreply.github.com>
2018-09-17 06:19:12 +0000

    Fixed dark pro

--- app/src/main/java/eu/faircode/email/FragmentSetup.java
+++ app/src/main/java/eu/faircode/email/FragmentSetup.java
@@ -210,6 +210,7 @@ public class FragmentSetup extends FragmentEx {
                         prefs.edit().putString("theme", checked ? "dark" : "light").apply();
                     }
                 } else {
+                    tbDarkTheme.setChecked(false);
                     FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
                     fragmentTransaction.replace(R.id.content_frame, new FragmentPro()).addToBackStack("pro");
                     fragmentTransaction.commit();
