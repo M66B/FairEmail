7e55dca15fc83865238cac170d28f07b03b4be0c
M66B <M66B@users.noreply.github.com>
2018-09-28 14:05:19 +0000

    Fixed threaded messages actions

--- app/src/main/java/eu/faircode/email/FragmentMessages.java
+++ app/src/main/java/eu/faircode/email/FragmentMessages.java
@@ -181,7 +181,7 @@ public class FragmentMessages extends FragmentEx {
                     return 0;
 
                 TupleMessageEx message = ((AdapterMessage) rvMessage.getAdapter()).getCurrentList().get(pos);
-                if (EntityFolder.OUTBOX.equals(message.folderType))
+                if (message.threaded || EntityFolder.OUTBOX.equals(message.folderType))
                     return 0;
 
                 return makeMovementFlags(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
