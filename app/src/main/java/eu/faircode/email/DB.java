940e7b390332a97a77030e19c115ae2a384e059f
M66B <M66B@users.noreply.github.com>
2018-10-29 10:46:49 +0000

    Fixed copyright
    Thanks @rancidfrog

--- app/src/main/java/eu/faircode/email/DB.java
+++ app/src/main/java/eu/faircode/email/DB.java
@@ -31,13 +31,13 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    NetGuard is distributed in the hope that it will be useful,
+    FairEmail is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with NetGuard.  If not, see <http://www.gnu.org/licenses/>.
+    along with FairEmail.  If not, see <http://www.gnu.org/licenses/>.
 
     Copyright 2018 by Marcel Bokhorst (M66B)
 */
