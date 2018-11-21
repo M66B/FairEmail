74169a4180a840567e4a5ec30993f0e50bb6b05c
M66B <M66B@users.noreply.github.com>
2018-11-21 13:03:55 +0100

    Revert "Throttle send rate"
    This reverts commit 285016124ee9779249f0bfaf11d90781c2ada214.

--- app/src/main/java/eu/faircode/email/ServiceSynchronize.java
+++ app/src/main/java/eu/faircode/email/ServiceSynchronize.java
@@ -138,7 +138,6 @@ public class ServiceSynchronize extends LifecycleService {
     private static final long RECONNECT_BACKOFF = 60 * 1000L; // milliseconds
     private static final int PREVIEW_SIZE = 250;
     private static final int ACCOUNT_ERROR_AFTER = 60; // minutes
-    private static final int SEND_WAIT = 90; // seconds
 
     static final int PI_WHY = 1;
     static final int PI_CLEAR = 2;
@@ -1558,13 +1557,6 @@ public class ServiceSynchronize extends LifecycleService {
             }
 
             EntityOperation.process(this);
-
-            // Throttle send rate
-            try {
-                Thread.sleep(SEND_WAIT * 60 * 1000L);
-            } catch (InterruptedException ignored) {
-            }
-
         } catch (MessagingException ex) {
             db.identity().setIdentityError(ident.id, Helper.formatThrowable(ex));
 
