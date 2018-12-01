a0935be3afec0e6f05a387f13889d4c3464d9e89
M66B <M66B@users.noreply.github.com>
2018-12-01 14:00:17 +0100

    Disable log auto scroll

--- app/src/main/java/eu/faircode/email/FragmentLogs.java
+++ app/src/main/java/eu/faircode/email/FragmentLogs.java
@@ -85,7 +85,7 @@ public class FragmentLogs extends FragmentEx {
                     logs = new ArrayList<>();
 
                 adapter.set(logs);
-                rvLog.scrollToPosition(0);
+                //rvLog.scrollToPosition(0);
 
                 pbWait.setVisibility(View.GONE);
                 grpReady.setVisibility(View.VISIBLE);
