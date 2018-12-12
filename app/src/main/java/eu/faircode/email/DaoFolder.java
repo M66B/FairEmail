8af8a0ee6d1dc3523cc15879813617bf8c6c6d23
M66B <M66B@users.noreply.github.com>
2018-12-12 09:04:00 +0100

    Allow sync of enabled accounts only

--- app/src/main/java/eu/faircode/email/DaoFolder.java
+++ app/src/main/java/eu/faircode/email/DaoFolder.java
@@ -42,7 +42,9 @@ public interface DaoFolder {
     @Query("SELECT * FROM folder WHERE synchronize")
     List<EntityFolder> getFoldersSynchronizing();
 
-    @Query("SELECT * FROM folder WHERE synchronize AND unified")
+    @Query("SELECT * FROM folder" +
+            " JOIN account ON account.id = folder.account" +
+            " WHERE account.synchronize AND folder.synchronize AND unified")
     List<EntityFolder> getFoldersSynchronizingUnified();
 
     @Query("SELECT * FROM folder" +
