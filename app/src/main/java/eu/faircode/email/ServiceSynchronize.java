3b8191785340ebe65a890dc73a79200ffa1553a5
M66B <M66B@users.noreply.github.com>
2019-02-20 08:18:01 +0000

    Silence IDLE exceptions

--- app/src/main/java/eu/faircode/email/ServiceSynchronize.java
+++ app/src/main/java/eu/faircode/email/ServiceSynchronize.java
@@ -1167,9 +1167,7 @@ public class ServiceSynchronize extends LifecycleService {
                                             ((IMAPFolder) ifolder).idle(false);
                                         }
                                     } catch (Throwable ex) {
-                                        Log.e(folder.name, ex);
-                                        reportError(account, folder, ex);
-                                        db.folder().setFolderError(folder.id, Helper.formatThrowable(ex));
+                                        Log.w(folder.name, ex);
                                         state.error();
                                     } finally {
                                         Log.i(folder.name + " end idle");
