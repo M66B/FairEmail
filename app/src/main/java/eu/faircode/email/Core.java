b2776f2c74e7f1c81ff7ad8cec443c4e285d4a27
M66B <M66B@users.noreply.github.com>
2019-02-28 08:27:08 +0000

    Reduce logging

--- app/src/main/java/eu/faircode/email/Core.java
+++ app/src/main/java/eu/faircode/email/Core.java
@@ -1269,7 +1269,7 @@ class Core {
 
             if (update)
                 db.message().updateMessage(message);
-            else
+            else if (BuildConfig.DEBUG)
                 Log.i(folder.name + " unchanged uid=" + uid);
         }
 
