a678e9292b7e41774e47167251473bf51bf80e9b
M66B <M66B@users.noreply.github.com>
2019-06-14 12:40:27 +0200

    Fix seek bar

--- app/src/main/java/eu/faircode/email/FragmentMessages.java
+++ app/src/main/java/eu/faircode/email/FragmentMessages.java
@@ -466,7 +466,7 @@ public class FragmentMessages extends FragmentBase implements SharedPreferences.
         seekBar.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                return false;
+                return true;
             }
         });
 
