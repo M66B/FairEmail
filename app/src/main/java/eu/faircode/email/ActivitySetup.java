4e557667bef0614a5f8cd0c5a48fa5a55070840a
M66B <M66B@users.noreply.github.com>
2019-08-18 09:21:54 +0200

    Cleanup

--- app/src/main/java/eu/faircode/email/ActivitySetup.java
+++ app/src/main/java/eu/faircode/email/ActivitySetup.java
@@ -140,12 +140,10 @@ public class ActivitySetup extends ActivityBase implements FragmentManager.OnBac
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.app_name, R.string.app_name) {
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(getString(R.string.app_name));
             }
 
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle(getString(R.string.app_name));
             }
         };
         drawerLayout.addDrawerListener(drawerToggle);
