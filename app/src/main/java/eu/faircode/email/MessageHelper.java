b33b72356dd8bcff36bfb8a162c18c2b35724acd
M66B <M66B@users.noreply.github.com>
2019-09-24 13:31:42 +0200

    Improved quote space handling

--- app/src/main/java/eu/faircode/email/MessageHelper.java
+++ app/src/main/java/eu/faircode/email/MessageHelper.java
@@ -908,13 +908,13 @@ public class MessageHelper {
                 String[] lines = result.split("\\r?\\n");
                 for (String line : lines) {
                     int tlevel = 0;
-                    while (line.startsWith(">")) {
+                    while (line.trim().startsWith(">")) {
                         tlevel++;
                         if (tlevel > level)
                             sb.append("<blockquote>");
-                        line = line.substring(1);
-                        if (line.startsWith(" "))
+                        while (line.startsWith(" "))
                             line = line.substring(1);
+                        line = line.substring(1);
                     }
                     for (int i = 0; i < level - tlevel; i++)
                         sb.append("</blockquote>");
