dev4223/open-source-email

Addes swipe snooze action

app/src/main/java/eu/faircode/email/FragmentAccount.java
@@ -145,6 +145,7 @@ public class FragmentAccount extends FragmentBase {
 
     static final Long SWIPE_ACTION_ASK = -1L;
     static final Long SWIPE_ACTION_SEEN = -2L;
+    static final Long SWIPE_ACTION_SNOOZE = -3L;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -1428,6 +1429,11 @@ public class FragmentAccount extends FragmentBase {
             seen.id = SWIPE_ACTION_SEEN;
             seen.name = getString(R.string.title_seen);
             folders.add(seen);
+
+            EntityFolder snooze = new EntityFolder();
+            snooze.id = SWIPE_ACTION_SNOOZE;
+            snooze.name = getString(R.string.title_snooze);
+            folders.add(snooze);
 
             folders.addAll(_folders);
 

app/src/main/java/eu/faircode/email/FragmentOptionsMisc.java
@@ -25,6 +25,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -34,6 +36,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,6 +50,7 @@ import androidx.preference.PreferenceManager;
 
 public class FragmentOptionsMisc extends FragmentBase implements SharedPreferences.OnSharedPreferenceChangeListener {
     private SwitchCompat swDoubleBack;
+    private EditText etDefaultSnooze;
     private Spinner spBiometricsTimeout;
     private SwitchCompat swEnglish;
     private SwitchCompat swWatchdog;
@@ -66,7 +70,7 @@ public class FragmentOptionsMisc extends FragmentBase implements SharedPreferenc
     private Group grpDebug;
 
     private final static String[] RESET_OPTIONS = new String[]{
-            "double_back", "biometrics_timeout", "english", "watchdog", "updates", "experiments", "crash_reports", "debug"
+            "double_back", "default_snooze", "biometrics_timeout", "english", "watchdog", "updates", "experiments", "crash_reports", "debug"
     };
 
     private final static String[] RESET_QUESTIONS = new String[]{
@@ -86,6 +90,7 @@ public class FragmentOptionsMisc extends FragmentBase implements SharedPreferenc
         // Get controls
 
         swDoubleBack = view.findViewById(R.id.swDoubleBack);
+        etDefaultSnooze = view.findViewById(R.id.etDefaultSnooze);
         spBiometricsTimeout = view.findViewById(R.id.spBiometricsTimeout);
         swEnglish = view.findViewById(R.id.swEnglish);
         swWatchdog = view.findViewById(R.id.swWatchdog);
@@ -114,6 +119,25 @@ public class FragmentOptionsMisc extends FragmentBase implements SharedPreferenc
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
                 prefs.edit().putBoolean("double_back", checked).apply();
+            }
+        });
+
+        etDefaultSnooze.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    int minutes = Integer.parseInt(editable.toString());
+                    prefs.edit().putInt("default_snooze", minutes).apply();
+                } catch (NumberFormatException ex) {
+                }
             }
         });
 
@@ -297,6 +321,7 @@ public class FragmentOptionsMisc extends FragmentBase implements SharedPreferenc
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
 
         swDoubleBack.setChecked(prefs.getBoolean("double_back", true));
+        etDefaultSnooze.setText(Integer.toString(prefs.getInt("default_snooze", 60)));
 
         int biometrics_timeout = prefs.getInt("biometrics_timeout", 2);
         int[] biometricTimeoutValues = getResources().getIntArray(R.array.biometricsTimeoutValues);

M66B/FairEmail

app/src/main/java/eu/faircode/email/FragmentAccount.java
@@ -145,6 +145,7 @@ public class FragmentAccount extends FragmentBase {
 
     static final Long SWIPE_ACTION_ASK = -1L;
     static final Long SWIPE_ACTION_SEEN = -2L;
+    static final Long SWIPE_ACTION_SNOOZE = -3L;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -1428,6 +1429,11 @@ public class FragmentAccount extends FragmentBase {
             seen.id = SWIPE_ACTION_SEEN;
             seen.name = getString(R.string.title_seen);
             folders.add(seen);
+
+            EntityFolder snooze = new EntityFolder();
+            snooze.id = SWIPE_ACTION_SNOOZE;
+            snooze.name = getString(R.string.title_snooze_now);
+            folders.add(snooze);
 
             folders.addAll(_folders);
 

app/src/main/java/eu/faircode/email/FragmentOptionsMisc.java
@@ -25,6 +25,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -34,6 +36,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,6 +50,7 @@ import androidx.preference.PreferenceManager;
 
 public class FragmentOptionsMisc extends FragmentBase implements SharedPreferences.OnSharedPreferenceChangeListener {
     private SwitchCompat swDoubleBack;
+    private EditText etDefaultSnooze;
     private Spinner spBiometricsTimeout;
     private SwitchCompat swEnglish;
     private SwitchCompat swWatchdog;
@@ -66,7 +70,7 @@ public class FragmentOptionsMisc extends FragmentBase implements SharedPreferenc
     private Group grpDebug;
 
     private final static String[] RESET_OPTIONS = new String[]{
-            "double_back", "biometrics_timeout", "english", "watchdog", "updates", "experiments", "crash_reports", "debug"
+            "double_back", "default_snooze", "biometrics_timeout", "english", "watchdog", "updates", "experiments", "crash_reports", "debug"
     };
 
     private final static String[] RESET_QUESTIONS = new String[]{
@@ -86,6 +90,7 @@ public class FragmentOptionsMisc extends FragmentBase implements SharedPreferenc
         // Get controls
 
         swDoubleBack = view.findViewById(R.id.swDoubleBack);
+        etDefaultSnooze = view.findViewById(R.id.etDefaultSnooze);
         spBiometricsTimeout = view.findViewById(R.id.spBiometricsTimeout);
         swEnglish = view.findViewById(R.id.swEnglish);
         swWatchdog = view.findViewById(R.id.swWatchdog);
@@ -114,6 +119,25 @@ public class FragmentOptionsMisc extends FragmentBase implements SharedPreferenc
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
                 prefs.edit().putBoolean("double_back", checked).apply();
+            }
+        });
+
+        etDefaultSnooze.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    int hours = Integer.parseInt(editable.toString());
+                    prefs.edit().putInt("default_snooze", hours).apply();
+                } catch (NumberFormatException ignored) {
+                }
             }
         });
 
@@ -297,6 +321,7 @@ public class FragmentOptionsMisc extends FragmentBase implements SharedPreferenc
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
 
         swDoubleBack.setChecked(prefs.getBoolean("double_back", true));
+        etDefaultSnooze.setText(Integer.toString(prefs.getInt("default_snooze", 1)));
 
         int biometrics_timeout = prefs.getInt("biometrics_timeout", 2);
         int[] biometricTimeoutValues = getResources().getIntArray(R.array.biometricsTimeoutValues);