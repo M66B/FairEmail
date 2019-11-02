5b3619c3702c2b389250b2997baadeb1a1d0e860
M66B <M66B@users.noreply.github.com>
2019-11-02 19:18:16 +0100

    Start view as new activity

--- app/src/main/java/eu/faircode/email/ActivityMain.java
+++ app/src/main/java/eu/faircode/email/ActivityMain.java
@@ -86,6 +86,7 @@ public class ActivityMain extends ActivityBase implements FragmentManager.OnBack
                         Log.logBundle(args);
 
                         Intent view = new Intent(ActivityMain.this, ActivityView.class);
+                        view.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         if (ACTION_REFRESH.equals(getIntent().getAction()))
                             view.putExtra("refresh", true);
 
