b854cbe8d35f8ca81a8f9f7f7e41ce04e614ed38
M66B <M66B@users.noreply.github.com>
2019-11-17 11:11:26 +0100

    Workaround mime types with removed dots

--- app/src/main/java/eu/faircode/email/EntityAttachment.java
+++ app/src/main/java/eu/faircode/email/EntityAttachment.java
@@ -160,13 +160,18 @@ public class EntityAttachment {
         if (extension == null)
             return type;
 
+        String gtype = MimeTypeMap.getSingleton()
+                .getMimeTypeFromExtension(extension.toLowerCase(Locale.ROOT));
+
+        // Some servers remove dots from mime types
+        if (gtype != null && gtype.replace(".", "").equals(type))
+            return gtype;
+
         if ("application/zip".equals(type) ||
                 "application/octet-stream".equals(type)) {
             if ("log".equalsIgnoreCase(extension))
                 return "text/plain";
 
-            String gtype = MimeTypeMap.getSingleton()
-                    .getMimeTypeFromExtension(extension.toLowerCase(Locale.ROOT));
             if (gtype == null || gtype.equals(type))
                 return type;
 
