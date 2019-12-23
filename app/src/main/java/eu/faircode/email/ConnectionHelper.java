fd223d6d4c9667b3e5a91d75311fb2d9726d8603
M66B <M66B@users.noreply.github.com>
2019-12-23 21:29:57 +0100

    Revert "Added network bug workaround"
    This reverts commit aabae2d8d627bd53b8c92790e1a9a85c21b25d35.

--- app/src/main/java/eu/faircode/email/ConnectionHelper.java
+++ app/src/main/java/eu/faircode/email/ConnectionHelper.java
@@ -187,13 +187,7 @@ public class ConnectionHelper {
         Network active = cm.getActiveNetwork();
         if (active == null) {
             Log.i("isMetered: no active network");
-            // Workaround bug in some Android versions
-            NetworkInfo ani = cm.getActiveNetworkInfo();
-            if (ani == null || !ani.isConnected()) {
-                Log.i("isMetered: no active network=" + (ani == null ? null : ani.isConnected()));
-                return null;
-            }
-            return cm.isActiveNetworkMetered();
+            return null;
         }
 
         // onLost [... state: DISCONNECTED/DISCONNECTED ... available: true]
