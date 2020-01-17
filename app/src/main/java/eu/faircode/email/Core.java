77e4a1d9cb1e3812b8167c449c24a1d9be596ca8
M66B <M66B@users.noreply.github.com>
2020-01-17 08:53:52 +0100

    Skip download of hidden messages

--- app/src/main/java/eu/faircode/email/Core.java
+++ app/src/main/java/eu/faircode/email/Core.java
@@ -1884,7 +1884,7 @@ class Core {
                                 istore, ifolder, (MimeMessage) isub[j],
                                 false, download,
                                 rules, state);
-                        ids[from + j] = (message == null ? null : message.id);
+                        ids[from + j] = (message == null || message.ui_hide ? null : message.id);
                     } catch (MessageRemovedException ex) {
                         Log.w(folder.name, ex);
                     } catch (FolderClosedException ex) {
