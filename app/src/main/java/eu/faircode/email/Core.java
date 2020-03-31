9a6bebe2a0215c7698a124fa13bf06460cea3d97
M66B <M66B@users.noreply.github.com>
2020-03-31 09:20:54 +0200

    Consider folder closed when retrying operation failed

--- app/src/main/java/eu/faircode/email/Core.java
+++ app/src/main/java/eu/faircode/email/Core.java
@@ -504,6 +504,8 @@ class Core {
 
             if (ops.size() == 0)
                 state.batchCompleted(folder.id, priority, sequence);
+            else
+                state.error(new FolderClosedException(ifolder));
         } finally {
             Log.i(folder.name + " end process state=" + state + " pending=" + ops.size());
         }
