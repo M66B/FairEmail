257928d4eea91e4ae0f84f44f20f1cae3485b91e
M66B <M66B@users.noreply.github.com>
2020-04-04 09:19:19 +0200

    Added message logging

--- app/src/main/java/eu/faircode/email/ApplicationEx.java
+++ app/src/main/java/eu/faircode/email/ApplicationEx.java
@@ -30,6 +30,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.util.Printer;
 import android.webkit.CookieManager;
 
 import androidx.lifecycle.Observer;
@@ -68,6 +69,13 @@ public class ApplicationEx extends Application {
 
         long start = new Date().getTime();
         Log.logMemory(this, "App create version=" + BuildConfig.VERSION_NAME);
+
+        getMainLooper().setMessageLogging(new Printer() {
+            @Override
+            public void println(String msg) {
+                Log.d("Loop: " + msg);
+            }
+        });
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         final boolean crash_reports = prefs.getBoolean("crash_reports", false);
