46aaaf3abf881c51258e8abdc7f7eaffeb570a6e
M66B <M66B@users.noreply.github.com>
2020-04-18 15:15:29 +0200

    Cleanup

--- app/src/main/java/eu/faircode/email/FixedEditText.java
+++ app/src/main/java/eu/faircode/email/FixedEditText.java
@@ -82,15 +82,8 @@ public class FixedEditText extends AppCompatEditText {
                     at android.view.View.performLongClick(View.java:7928)
                     at android.view.View$CheckForLongPress.run(View.java:29321)
 */
-/*
-            java.lang.NullPointerException: Attempt to invoke virtual method 'int android.widget.Editor$SelectionModifierCursorController.getMinTouchOffset()' on a null object reference
-                    at android.widget.Editor.touchPositionIsInSelection(Unknown:36)
-                    at android.widget.Editor.performLongClick(Unknown:72)
-                    at android.widget.TextView.performLongClick(Unknown:24)
-*/
             Log.w(ex);
             return false;
         }
     }
-
 }
