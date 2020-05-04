e84f9b0f094fa8340072d53cf2f1b6f3e5430eae
M66B <M66B@users.noreply.github.com>
2020-05-04 19:45:47 +0200

    Fixed aligning tables

--- app/src/main/java/eu/faircode/email/HtmlHelper.java
+++ app/src/main/java/eu/faircode/email/HtmlHelper.java
@@ -455,7 +455,7 @@ public class HtmlHelper {
         if (text_color)
             whitelist.addAttributes("font", "color");
         if (text_align)
-            whitelist.addTags("center").addAttributes(":all", "align");
+            whitelist.addTags("center");
         if (!view)
             whitelist.addProtocols("img", "src", "content");
 
@@ -519,11 +519,6 @@ public class HtmlHelper {
                     style = mergeStyles(style, "text-align:center");
                     element.tagName("div");
                 }
-
-                // https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes
-                String align = element.attr("align");
-                if (!TextUtils.isEmpty(align))
-                    style = mergeStyles(style, "text-align:" + align);
             }
 
             // Process style
