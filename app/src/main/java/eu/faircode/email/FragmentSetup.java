7547e747766235d54507262758f64d3e6852d122
M66B <M66B@users.noreply.github.com>
2020-05-09 22:25:40 +0200

    Removed OAuth limitations

--- app/src/main/java/eu/faircode/email/FragmentSetup.java
+++ app/src/main/java/eu/faircode/email/FragmentSetup.java
@@ -168,16 +168,14 @@ public class FragmentSetup extends FragmentBase {
                 int order = 1;
                 popupMenu.getMenu().add(Menu.NONE, R.string.title_setup_gmail, order++, R.string.title_setup_gmail);
 
-                if (!Helper.isPlayStoreInstall() &&
-                        (Helper.hasValidFingerprint(getContext()) || BuildConfig.DEBUG))
-                    for (EmailProvider provider : EmailProvider.loadProfiles(getContext()))
-                        if (provider.oauth != null && (provider.oauth.enabled || BuildConfig.DEBUG))
-                            popupMenu.getMenu()
-                                    .add(Menu.NONE, -1, order++, getString(R.string.title_setup_oauth, provider.name))
-                                    .setIntent(new Intent(ActivitySetup.ACTION_QUICK_OAUTH)
-                                            .putExtra("id", provider.id)
-                                            .putExtra("name", provider.name)
-                                            .putExtra("askAccount", provider.oauth.askAccount));
+                for (EmailProvider provider : EmailProvider.loadProfiles(getContext()))
+                    if (provider.oauth != null && (provider.oauth.enabled || BuildConfig.DEBUG))
+                        popupMenu.getMenu()
+                                .add(Menu.NONE, -1, order++, getString(R.string.title_setup_oauth, provider.name))
+                                .setIntent(new Intent(ActivitySetup.ACTION_QUICK_OAUTH)
+                                        .putExtra("id", provider.id)
+                                        .putExtra("name", provider.name)
+                                        .putExtra("askAccount", provider.oauth.askAccount));
 
                 //popupMenu.getMenu().add(Menu.NONE, R.string.title_setup_activesync, order++, R.string.title_setup_activesync);
                 popupMenu.getMenu().add(Menu.NONE, R.string.title_setup_other, order++, R.string.title_setup_other);
