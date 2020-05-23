a4731b984fe3b75005386c930fbc4aac29b38dd6
M66B <M66B@users.noreply.github.com>
2020-05-23 20:47:29 +0200

    Single finger pan web view

--- app/src/main/java/eu/faircode/email/AdapterMessage.java
+++ app/src/main/java/eu/faircode/email/AdapterMessage.java
@@ -2624,8 +2624,17 @@ public class AdapterMessage extends RecyclerView.Adapter<AdapterMessage.ViewHold
                 } else
                     return false;
             } else {
-                view.getParent().requestDisallowInterceptTouchEvent(false);
-                return (view.getId() == R.id.wvBody && ev.getAction() == MotionEvent.ACTION_MOVE);
+                if (view.getId() == R.id.tvBody)
+                    view.getParent().requestDisallowInterceptTouchEvent(false);
+                else {
+                    boolean xm = view.canScrollHorizontally(-1);
+                    boolean xp = view.canScrollHorizontally(1);
+                    boolean ym = view.canScrollVertically(-1);
+                    boolean yp = view.canScrollVertically(1);
+                    boolean zoomed = (xm || xp || ym || yp);
+                    view.getParent().requestDisallowInterceptTouchEvent(zoomed);
+                }
+                return false;
             }
         }
 
