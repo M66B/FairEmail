ee3b6df16d0b4ed0f5a5bb66e2aa7e6554437a44
M66B <M66B@users.noreply.github.com>
2020-06-15 15:49:26 +0200

    Retry loading initial page

--- app/src/main/java/androidx/room/paging/LimitOffsetDataSource.java
+++ app/src/main/java/androidx/room/paging/LimitOffsetDataSource.java
@@ -114,27 +114,38 @@ public abstract class LimitOffsetDataSource<T> extends PositionalDataSource<T> {
         int firstLoadPosition = 0;
         RoomSQLiteQuery sqLiteQuery = null;
         Cursor cursor = null;
-        mDb.beginTransaction();
-        try {
-            totalCount = countItems();
-            if (totalCount != 0) {
-                // bound the size requested, based on known count
-                firstLoadPosition = computeInitialLoadPosition(params, totalCount);
-                int firstLoadSize = computeInitialLoadSize(params, firstLoadPosition, totalCount);
+        int retry = 0;
+        while (cursor == null) {
+            mDb.beginTransaction();
+            try {
+                totalCount = countItems();
+                if (totalCount != 0) {
+                    // bound the size requested, based on known count
+                    firstLoadPosition = computeInitialLoadPosition(params, totalCount);
+                    int firstLoadSize = computeInitialLoadSize(params, firstLoadPosition, totalCount);
 
-                sqLiteQuery = getSQLiteQuery(firstLoadPosition, firstLoadSize);
-                cursor = mDb.query(sqLiteQuery);
-                List<T> rows = convertRows(cursor);
-                mDb.setTransactionSuccessful();
-                list = rows;
-            }
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
-            mDb.endTransaction();
-            if (sqLiteQuery != null) {
-                sqLiteQuery.release();
+                    sqLiteQuery = getSQLiteQuery(firstLoadPosition, firstLoadSize);
+                    cursor = mDb.query(sqLiteQuery);
+                    List<T> rows = convertRows(cursor);
+                    mDb.setTransactionSuccessful();
+                    list = rows;
+                }
+            } catch (Throwable ex) {
+                if (++retry > 10)
+                    throw ex;
+                eu.faircode.email.Log.w(ex);
+                try {
+                    Thread.sleep(3000L);
+                } catch (InterruptedException ignored) {
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+                mDb.endTransaction();
+                if (sqLiteQuery != null) {
+                    sqLiteQuery.release();
+                }
             }
         }
 
