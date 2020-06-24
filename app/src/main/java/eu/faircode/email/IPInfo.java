797985849a290ef04b641f1dc86b301370925e35
M66B <M66B@users.noreply.github.com>
2020-06-24 11:29:13 +0200

    Added comment

--- app/src/main/java/eu/faircode/email/IPInfo.java
+++ app/src/main/java/eu/faircode/email/IPInfo.java
@@ -81,6 +81,18 @@ public class IPInfo {
         Organization organization = new Organization();
         try {
             String json = Helper.readStream(connection.getInputStream(), StandardCharsets.UTF_8.name());
+            /* {
+                "ip": "8.8.8.8",
+                    "hostname": "dns.google",
+                    "city": "Mountain View",
+                    "region": "California",
+                    "country": "US",
+                    "loc": "37.4056,-122.0775",
+                    "org": "AS15169 Google LLC",
+                    "postal": "94043",
+                    "timezone": "America/Los_Angeles"
+            } */
+
             JSONObject jinfo = new JSONObject(json);
             organization.name = jinfo.optString("org");
             organization.country = jinfo.optString("country");
