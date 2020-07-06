a24400c949de686182b76f1f7df33b4394c391ad
M66B <M66B@users.noreply.github.com>
2020-07-06 18:30:10 +0200

    Added copyright

--- app/src/main/java/eu/faircode/email/ToastEx.java
+++ app/src/main/java/eu/faircode/email/ToastEx.java
@@ -1,5 +1,24 @@
 package eu.faircode.email;
 
+/*
+    This file is part of FairEmail.
+
+    FairEmail is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FairEmail is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FairEmail.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2018-2020 by Marcel Bokhorst (M66B)
+*/
+
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Looper;
