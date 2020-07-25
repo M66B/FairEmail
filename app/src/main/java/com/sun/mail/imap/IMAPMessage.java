d83db8be77f04dee7e46db5cc9f1e481d429bdf8
M66B <M66B@users.noreply.github.com>
2020-07-25 13:23:03 +0200

    Improved workaround for expunged messages

--- app/src/main/java/com/sun/mail/imap/IMAPMessage.java
+++ app/src/main/java/com/sun/mail/imap/IMAPMessage.java
@@ -1697,4 +1697,26 @@ public class IMAPMessage extends MimeMessage implements ReadableMime {
     Session _getSession() {
 	return session;
     }
+
+    @Override
+    public boolean isExpunged() {
+        if (super.isExpunged())
+            return true;
+
+        // Workaround expunged messages without deleted flag
+        if (envelope != null &&
+                envelope.date == null &&
+                envelope.subject == null &&
+                envelope.from == null &&
+                envelope.sender == null &&
+                envelope.replyTo == null &&
+                envelope.to == null &&
+                envelope.cc == null &&
+                envelope.inReplyTo == null &&
+                envelope.messageId == null &&
+                headersLoaded && loadedHeaders.size() == 0)
+            return true;
+
+        return false;
+    }
 }
