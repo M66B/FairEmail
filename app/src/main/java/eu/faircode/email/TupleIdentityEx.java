1dee158194e30ab001d131301b1757203f033253
M66B <M66B@users.noreply.github.com>
2020-08-20 22:54:14 +0200

    Removed drafts warning from identities

--- app/src/main/java/eu/faircode/email/TupleIdentityEx.java
+++ app/src/main/java/eu/faircode/email/TupleIdentityEx.java
@@ -23,15 +23,13 @@ import java.util.Objects;
 
 public class TupleIdentityEx extends EntityIdentity {
     public String accountName;
-    public Long drafts;
 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof TupleIdentityEx) {
             TupleIdentityEx other = (TupleIdentityEx) obj;
             return (super.equals(obj) &&
-                    Objects.equals(accountName, other.accountName) &&
-                    Objects.equals(drafts, other.drafts));
+                    Objects.equals(accountName, other.accountName));
         } else
             return false;
     }
