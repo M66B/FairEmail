a443f1299f0c2ae62e23f011d7ea46d0cd98a423
M66B <M66B@users.noreply.github.com>
2020-10-12 16:38:57 +0200

    Small improvement

--- app/src/main/java/eu/faircode/email/EntityOperation.java
+++ app/src/main/java/eu/faircode/email/EntityOperation.java
@@ -453,10 +453,8 @@ public class EntityOperation {
             try {
                 JSONArray jargs = new JSONArray(args);
                 long tmpid = jargs.optLong(2, -1);
-                if (tmpid < 0)
-                    return;
-
-                db.message().deleteMessage(tmpid);
+                if (tmpid > 0)
+                    db.message().deleteMessage(tmpid);
             } catch (JSONException ex) {
                 Log.e(ex);
             }
