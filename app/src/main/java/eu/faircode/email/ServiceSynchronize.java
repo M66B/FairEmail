3cbba021e50546da8ad64450398e182fc77f5c52
M66B <M66B@users.noreply.github.com>
2020-11-10 20:06:02 +0100

    Added fetch yield

--- app/src/main/java/eu/faircode/email/ServiceSynchronize.java
+++ app/src/main/java/eu/faircode/email/ServiceSynchronize.java
@@ -119,6 +119,7 @@ public class ServiceSynchronize extends ServiceBase implements SharedPreferences
     private static final int ACCOUNT_ERROR_AFTER_POLL = 4; // times
     private static final int BACKOFF_ERROR_AFTER = 16; // seconds
     private static final int FAST_FAIL_THRESHOLD = 75; // percent
+    private static final int FETCH_YIELD_DURATION = 50; // milliseconds
 
     private static final String ACTION_NEW_MESSAGE_COUNT = BuildConfig.APPLICATION_ID + ".NEW_MESSAGE_COUNT";
 
@@ -1175,6 +1176,8 @@ public class ServiceSynchronize extends ServiceBase implements SharedPreferences
                                         } finally {
                                             db.endTransaction();
                                         }
+
+                                        Thread.sleep(FETCH_YIELD_DURATION);
                                     } catch (Throwable ex) {
                                         Log.e(folder.name, ex);
                                         EntityLog.log(
@@ -1204,6 +1207,8 @@ public class ServiceSynchronize extends ServiceBase implements SharedPreferences
                                         } finally {
                                             db.endTransaction();
                                         }
+
+                                        Thread.sleep(FETCH_YIELD_DURATION);
                                     } catch (Throwable ex) {
                                         Log.e(folder.name, ex);
                                         EntityLog.log(
@@ -1228,6 +1233,8 @@ public class ServiceSynchronize extends ServiceBase implements SharedPreferences
 
                                         long uid = ifolder.getUID(e.getMessage());
                                         EntityOperation.queue(ServiceSynchronize.this, folder, EntityOperation.FETCH, uid);
+
+                                        Thread.sleep(FETCH_YIELD_DURATION);
                                     } catch (Throwable ex) {
                                         Log.e(folder.name, ex);
                                         EntityLog.log(
