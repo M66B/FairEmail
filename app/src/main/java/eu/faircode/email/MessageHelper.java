16e890e0a160d4144b7bf7690999bc93e214a344
M66B <M66B@users.noreply.github.com>
2020-11-19 08:39:10 +0100

    Auto fix incorrect UTF-16

--- app/src/main/java/eu/faircode/email/MessageHelper.java
+++ app/src/main/java/eu/faircode/email/MessageHelper.java
@@ -1790,6 +1790,24 @@ public class MessageHelper {
                         result = HtmlHelper.flow(result);
                     result = "<div x-plain=\"true\">" + HtmlHelper.formatPre(result) + "</div>";
                 } else if (h.part.isMimeType("text/html")) {
+                    // Fix incorrect UTF16
+                    if (charset != null)
+                        try {
+                            Charset c = Charset.forName(charset);
+                            if (CHARSET16.contains(c)) {
+                                Charset detected = CharsetHelper.detect(result);
+                                if (!CHARSET16.contains(detected))
+                                    Log.e(new Throwable("Charset=" + c + " detected=" + detected));
+                                if (StandardCharsets.US_ASCII.equals(detected) ||
+                                        StandardCharsets.UTF_8.equals(detected)) {
+                                    charset = null;
+                                    result = new String(result.getBytes(c), detected);
+                                }
+                            }
+                        } catch (Throwable ex) {
+                            Log.w(ex);
+                        }
+
                     if (charset == null) {
                         // <meta charset="utf-8" />
                         // <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
