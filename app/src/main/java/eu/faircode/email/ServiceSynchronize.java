8a08e31b08b84b76fda8e4cdfed66a16717a3cff
M66B <M66B@users.noreply.github.com>
2020-12-25 16:17:49 +0100

    Skip idle for transient accounts/folders

--- app/src/main/java/eu/faircode/email/ServiceSynchronize.java
+++ app/src/main/java/eu/faircode/email/ServiceSynchronize.java
@@ -1288,7 +1288,7 @@ public class ServiceSynchronize extends ServiceBase implements SharedPreferences
                             });
 
                             // Idle folder
-                            if (!isTransient(account)) {
+                            if (!isTransient(account) && folder.synchronize) {
                                 Thread idler = new Thread(new Runnable() {
                                     @Override
                                     public void run() {
