e3b64376e00661be4c904d8ba391fa60584f171d
M66B <M66B@users.noreply.github.com>
2020-12-28 08:31:35 +0100

    Revert "Always show unseen archived messages"
    This reverts commit bb1b6b3b6384ff3611b072daa5bbdba72154d214.

--- app/src/main/java/eu/faircode/email/DaoMessage.java
+++ app/src/main/java/eu/faircode/email/DaoMessage.java
@@ -218,7 +218,6 @@ public interface DaoMessage {
             " AND message.thread = :thread" +
             " AND (:id IS NULL OR message.id = :id)" +
             " AND (NOT :filter_archive" +
-            "  OR (NOT message.ui_seen AND NOT message.ui_hide)" +
             "  OR folder.type <> '" + EntityFolder.ARCHIVE + "'" +
             "  OR NOT EXISTS" +
             "   (SELECT * FROM message m" +
