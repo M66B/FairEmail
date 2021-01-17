380fa06dbea65f025788dbf040aeebd164c5eca3
M66B <M66B@users.noreply.github.com>
2021-01-17 14:47:24 +0100

    Incognito mode for email addresses

---
+++ app/src/main/java/eu/faircode/email/EditTextMultiAutoComplete.java
@@ -0,0 +1,44 @@
+package eu.faircode.email;
+
+/*
+    This file is part of FairEmail.
+
+    FairEmail is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FairEmail is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FairEmail.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2018-2021 by Marcel Bokhorst (M66B)
+*/
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatMultiAutoCompleteTextView;
+
+public class EditTextMultiAutoComplete extends AppCompatMultiAutoCompleteTextView {
+    public EditTextMultiAutoComplete(@NonNull Context context) {
+        super(context);
+        Helper.setKeyboardIncognitoMode(this, context);
+    }
+
+    public EditTextMultiAutoComplete(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        Helper.setKeyboardIncognitoMode(this, context);
+    }
+
+    public EditTextMultiAutoComplete(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        Helper.setKeyboardIncognitoMode(this, context);
+    }
+}
