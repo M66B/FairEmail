bc4cb29839e8420021657afff4c9ec4370441419
M66B <M66B@users.noreply.github.com>
2021-01-18 22:16:32 +0100

    Small fix

--- app/src/main/java/eu/faircode/email/TextHelper.java
+++ app/src/main/java/eu/faircode/email/TextHelper.java
@@ -1,5 +1,24 @@
 package eu.faircode.email;
 
+/*
+    This file is part of FairEmail.
+
+    FairEmail is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FairEmail is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FairEmail.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2018-2021 by Marcel Bokhorst (M66B)
+*/
+
 import android.app.Person;
 import android.content.Context;
 import android.os.Build;
@@ -22,26 +41,6 @@ import java.util.Locale;
 import java.util.Set;
 
 public class TextHelper {
-
-/*
-    This file is part of FairEmail.
-
-    FairEmail is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    FairEmail is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with FairEmail.  If not, see <http://www.gnu.org/licenses/>.
-
-    Copyright 2018-2021 by Marcel Bokhorst (M66B)
-*/
-
     static Locale detectLanguage(Context context, String text) {
         // Why not ML kit? https://developers.google.com/ml-kit/terms
         if (TextUtils.isEmpty(text))
