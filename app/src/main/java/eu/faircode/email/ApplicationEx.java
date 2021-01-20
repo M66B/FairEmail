658c95fd2231c444085cabd811ebbc5ce9319f1f
M66B <M66B@users.noreply.github.com>
2021-01-20 09:20:41 +0100

    Cleanup

--- app/src/main/java/eu/faircode/email/ApplicationEx.java
+++ app/src/main/java/eu/faircode/email/ApplicationEx.java
@@ -152,14 +152,6 @@ public class ApplicationEx extends Application
 
         registerReceiver(onScreenOff, new IntentFilter(Intent.ACTION_SCREEN_OFF));
 
-        if (BuildConfig.DEBUG)
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    DnsHelper.test(ApplicationEx.this);
-                }
-            }).start();
-
         long end = new Date().getTime();
         Log.i("App created " + (end - start) + " ms");
     }
