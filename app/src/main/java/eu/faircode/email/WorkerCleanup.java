d2ab30734fed3b4774574f6abcf9ec43674dfe67
M66B <M66B@users.noreply.github.com>
2021-02-22 18:20:39 +0100

    Fixed cleanup cached images

--- app/src/main/java/eu/faircode/email/WorkerCleanup.java
+++ app/src/main/java/eu/faircode/email/WorkerCleanup.java
@@ -257,7 +257,7 @@ public class WorkerCleanup extends Worker {
             if (images != null)
                 for (File file : images)
                     if (manual || file.lastModified() + KEEP_FILES_DURATION < now) {
-                        long id = Long.parseLong(file.getName().split("_")[0]);
+                        long id = Long.parseLong(file.getName().split("[_\\.]")[0]);
                         EntityMessage message = db.message().getMessage(id);
                         if (manual || message == null ||
                                 file.lastModified() + KEEP_IMAGES_DURATION < now) {
