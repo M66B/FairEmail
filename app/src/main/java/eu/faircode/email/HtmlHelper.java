e21e3fc8cbcd08e9314ae394bfe669bac74dcdf1
M66B <M66B@users.noreply.github.com>
2021-03-05 13:48:16 +0100

    Allow all link protocols

--- app/src/main/java/eu/faircode/email/HtmlHelper.java
+++ app/src/main/java/eu/faircode/email/HtmlHelper.java
@@ -434,8 +434,7 @@ public class HtmlHelper {
                 .removeAttributes("th", "colspan", "rowspan", "width")
                 .addProtocols("img", "src", "cid")
                 .addProtocols("img", "src", "data")
-                .removeProtocols("a", "href", "ftp")
-                .addProtocols("a", "href", "full", "xmpp", "geo", "tel");
+                .removeTags("a").addAttributes("a", "href", "title");
         if (text_color)
             whitelist.addAttributes("font", "color");
         if (text_size)
