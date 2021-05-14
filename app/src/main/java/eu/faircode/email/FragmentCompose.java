5e5ba013b5c1f4be6b14d7f3752a9eef93596445
M66B <M66B@users.noreply.github.com>
2021-05-14 21:08:43 +0200

    Disabled auto pos

--- app/src/main/java/eu/faircode/email/FragmentCompose.java
+++ app/src/main/java/eu/faircode/email/FragmentCompose.java
@@ -5696,6 +5696,9 @@ public class FragmentCompose extends FragmentBase {
     }
 
     private int getAutoPos(int start, int end) {
+        if (!BuildConfig.DEBUG)
+            return -1;
+
         if (start > end || end == 0)
             return -1;
 
