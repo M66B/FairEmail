6389596ed7a02b9d337a66b2c32443ec01fa422e
M66B <M66B@users.noreply.github.com>
2021-07-02 19:22:46 +0200

    HTTP

--- app/src/main/java/eu/faircode/email/FragmentDialogOpenLink.java
+++ app/src/main/java/eu/faircode/email/FragmentDialogOpenLink.java
@@ -191,6 +191,8 @@ public class FragmentDialogOpenLink extends FragmentDialogBase {
 
                 cbSecure.setText(
                         secure ? R.string.title_link_https : R.string.title_link_http);
+                cbSecure.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                        0, 0, secure ? 0 : R.drawable.twotone_http_24, 0);
                 cbSecure.setTextColor(Helper.resolveColor(context,
                         secure ? android.R.attr.textColorSecondary : R.attr.colorWarning));
                 cbSecure.setTypeface(
