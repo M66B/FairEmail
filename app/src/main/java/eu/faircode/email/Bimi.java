b3e48e417cb9b65634523f63dd3982d0cc49920b
M66B <M66B@users.noreply.github.com>
2021-07-17 19:15:40 +0200

    Reduced logging

--- app/src/main/java/eu/faircode/email/Bimi.java
+++ app/src/main/java/eu/faircode/email/Bimi.java
@@ -37,6 +37,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.KeyStore;
 import java.security.cert.CertPathBuilder;
@@ -297,6 +298,8 @@ public class Bimi {
 
                         Log.i("BIMI valid domain=" + domain);
                         verified = true;
+                    } catch (MalformedURLException ex) {
+                        Log.i(ex);
                     } catch (Throwable ex) {
                         Log.w(new Throwable("BIMI " + domain, ex));
                     }
