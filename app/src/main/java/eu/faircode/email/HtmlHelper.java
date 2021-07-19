51f1b0b8f4bdc31eba36368d49b328cbdc8514c1
M66B <M66B@users.noreply.github.com>
2021-07-19 11:18:36 +0200

    Oops

--- app/src/main/java/eu/faircode/email/HtmlHelper.java
+++ app/src/main/java/eu/faircode/email/HtmlHelper.java
@@ -1675,7 +1675,7 @@ public class HtmlHelper {
     static String encodeWebColor(int color) {
         int alpha = Color.alpha(color);
         int rgb = 0xFFFFFF & color;
-        if (alpha == 0)
+        if (alpha == 255)
             return String.format("#%06X", rgb);
         else
             return String.format("#%06X%02X", rgb, alpha);
