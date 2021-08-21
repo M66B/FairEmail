d96e979731f7fe4a07a8a25664ca7368492efc67
M66B <M66B@users.noreply.github.com>
2021-08-21 11:06:05 +0200

    Set locus ID

--- app/src/main/java/eu/faircode/email/Shortcuts.java
+++ app/src/main/java/eu/faircode/email/Shortcuts.java
@@ -41,6 +41,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.Person;
+import androidx.core.content.LocusIdCompat;
 import androidx.core.content.pm.ShortcutInfoCompat;
 import androidx.core.content.pm.ShortcutManagerCompat;
 import androidx.core.graphics.drawable.IconCompat;
@@ -303,11 +304,16 @@ class Shortcuts {
 
         IconCompat icon = IconCompat.createWithBitmap(bm);
         String id = "message:" + message.id;
-        return new ShortcutInfoCompat.Builder(context, id)
+        ShortcutInfoCompat.Builder builder = new ShortcutInfoCompat.Builder(context, id)
                 .setIcon(icon)
                 .setShortLabel(label)
                 .setLongLabel(label)
                 .setIntent(thread);
+
+        if (message.thread != null)
+            builder.setLocusId(new LocusIdCompat(message.thread));
+
+        return builder;
     }
 
     @NonNull
