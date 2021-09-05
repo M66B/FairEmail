8744efd7d52c8143b80848dd7084583036d51572
M66B <M66B@users.noreply.github.com>
2021-09-05 07:42:08 +0200

    Formatting

--- app/src/main/java/eu/faircode/email/ServiceSynchronize.java
+++ app/src/main/java/eu/faircode/email/ServiceSynchronize.java
@@ -742,8 +742,8 @@ public class ServiceSynchronize extends ServiceBase implements SharedPreferences
                 if (lastCount == null || !lastCount.equals(count)) {
                     lastCount = count;
 
-                    EntityLog.log(ServiceSynchronize.this, " Badge count=" + count +
-                            "enabled=" + badge + " Unseen/ignored=" + unseen_ignored);
+                    EntityLog.log(ServiceSynchronize.this, "Badge count=" + count +
+                            " enabled=" + badge + " Unseen/ignored=" + unseen_ignored);
 
                     // Broadcast new message count
                     try {
