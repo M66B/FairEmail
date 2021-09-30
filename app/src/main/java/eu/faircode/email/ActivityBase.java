fb9b0a1c4293f96fba84fc15e7199df569a6dc07
M66B <M66B@users.noreply.github.com>
2021-09-30 11:38:55 +0200

    Conditionally clear

--- app/src/main/java/eu/faircode/email/ActivityBase.java
+++ app/src/main/java/eu/faircode/email/ActivityBase.java
@@ -766,19 +766,20 @@ abstract class ActivityBase extends AppCompatActivity implements SharedPreferenc
         @Override
         public void onFragmentDestroyed(@NonNull FragmentManager fm, @NonNull Fragment f) {
             log(fm, f, "onFragmentDestroyed");
-            try {
-                for (Field field : f.getClass().getDeclaredFields()) {
-                    Class<?> type = field.getType();
-                    if (View.class.isAssignableFrom(type) ||
-                            RecyclerView.Adapter.class.isAssignableFrom(type)) {
-                        Log.i("Clearing " + f.getClass().getSimpleName() + ":" + field.getName());
-                        field.setAccessible(true);
-                        field.set(f, null);
+            if (!BuildConfig.PLAY_STORE_RELEASE)
+                try {
+                    for (Field field : f.getClass().getDeclaredFields()) {
+                        Class<?> type = field.getType();
+                        if (View.class.isAssignableFrom(type) ||
+                                RecyclerView.Adapter.class.isAssignableFrom(type)) {
+                            Log.i("Clearing " + f.getClass().getSimpleName() + ":" + field.getName());
+                            field.setAccessible(true);
+                            field.set(f, null);
+                        }
                     }
+                } catch (Throwable ex) {
+                    Log.w(ex);
                 }
-            } catch (Throwable ex) {
-                Log.w(ex);
-            }
         }
 
         @Override
