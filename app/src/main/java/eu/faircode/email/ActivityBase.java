6995682610ef63088d925983ee5b3a3168e5947c
M66B <M66B@users.noreply.github.com>
2021-10-01 16:58:33 +0200

    Enable cleaning for Play store version

--- app/src/main/java/eu/faircode/email/ActivityBase.java
+++ app/src/main/java/eu/faircode/email/ActivityBase.java
@@ -766,20 +766,19 @@ abstract class ActivityBase extends AppCompatActivity implements SharedPreferenc
         @Override
         public void onFragmentDestroyed(@NonNull FragmentManager fm, @NonNull Fragment f) {
             log(fm, f, "onFragmentDestroyed");
-            if (!BuildConfig.PLAY_STORE_RELEASE)
-                try {
-                    for (Field field : f.getClass().getDeclaredFields()) {
-                        Class<?> type = field.getType();
-                        if (View.class.isAssignableFrom(type) ||
-                                RecyclerView.Adapter.class.isAssignableFrom(type)) {
-                            Log.i("Clearing " + f.getClass().getSimpleName() + ":" + field.getName());
-                            field.setAccessible(true);
-                            field.set(f, null);
-                        }
+            try {
+                for (Field field : f.getClass().getDeclaredFields()) {
+                    Class<?> type = field.getType();
+                    if (View.class.isAssignableFrom(type) ||
+                            RecyclerView.Adapter.class.isAssignableFrom(type)) {
+                        Log.i("Clearing " + f.getClass().getSimpleName() + ":" + field.getName());
+                        field.setAccessible(true);
+                        field.set(f, null);
                     }
-                } catch (Throwable ex) {
-                    Log.w(ex);
                 }
+            } catch (Throwable ex) {
+                Log.w(ex);
+            }
         }
 
         @Override
