afd67cde4f17e677ed9bc330da5a8ce4c2b75909
M66B <M66B@users.noreply.github.com>
2021-12-11 09:04:37 +0100

    Show no authentication

--- app/src/main/java/eu/faircode/email/AdapterMessage.java
+++ app/src/main/java/eu/faircode/email/AdapterMessage.java
@@ -1212,7 +1212,7 @@ public class AdapterMessage extends RecyclerView.Adapter<AdapterMessage.ViewHold
                 ibAuth.setImageLevel(auths + 1);
                 ibAuth.setImageTintList(ColorStateList.valueOf(
                         auths < 3 ? colorControlNormal : colorVerified));
-                ibAuth.setVisibility(auths > 0 ? View.VISIBLE : View.GONE);
+                ibAuth.setVisibility(View.VISIBLE);
             } else
                 ibAuth.setVisibility(View.GONE);
 
