497c60a708d7ed2309133e6a6cc3ca77aa596211
M66B <M66B@users.noreply.github.com>
2022-01-08 11:42:04 +0100

    Store BCC address in local contact database

--- app/src/main/java/eu/faircode/email/EntityContact.java
+++ app/src/main/java/eu/faircode/email/EntityContact.java
@@ -159,6 +159,8 @@ public class EntityContact implements Serializable {
                 addresses.addAll(Arrays.asList(message.to));
             if (message.cc != null)
                 addresses.addAll(Arrays.asList(message.cc));
+            if (message.bcc != null)
+                addresses.addAll(Arrays.asList(message.bcc));
         }
 
         update(context, folder.account, addresses.toArray(new Address[0]), type, message.received);
