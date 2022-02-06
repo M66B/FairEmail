b9235fef3b027acc8f22ae1e4b0824a62e75a80c
M66B <M66B@users.noreply.github.com>
2022-02-06 09:54:17 +0100

    Added remark

--- app/src/main/java/eu/faircode/email/Core.java
+++ app/src/main/java/eu/faircode/email/Core.java
@@ -624,6 +624,7 @@ class Core {
                             // Fetch UID: NO [UNAVAILABLE] Failed to open mailbox
                             // Fetch UID: NO [TEMPFAIL] SELECT completed
                             // Fetch UID: NO Internal error. Try again later... (MARKER:xxx)
+                            // Fetch UID: BAD Serious error while processing UID FETCH (NioRecvFail (nn/nn))
                             // Move: NO Over quota
                             // Move: NO No matching messages
                             // Move: NO [EXPUNGEISSUED] Some of the requested messages no longer exist (n.nnn + n.nnn + n.nnn secs)
