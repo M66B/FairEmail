e72cd201117ef91acaeaf9a87cefce4d3933d26f
M66B <M66B@users.noreply.github.com>
2022-02-19 10:37:09 +0100

    Debug Open Sans

--- app/src/main/java/eu/faircode/email/StyleHelper.java
+++ app/src/main/java/eu/faircode/email/StyleHelper.java
@@ -931,6 +931,8 @@ public class StyleHelper {
         // https://developer.mozilla.org/en-US/docs/Web/CSS/font-family
         // TODO: Microsoft: Georgia (Serif), Tahoma (Sans-serif), Trebuchet MS (Sans-serif)
         String faces = family.toLowerCase(Locale.ROOT);
+        if (faces.contains("open sans"))
+            return "Open Sans, Arial, Verdana, Helvetica, Helvetica Neue, sans-serif";
         if (faces.contains("arimo"))
             return "Arimo, Arial, Verdana, Helvetica, Helvetica Neue, sans-serif";
         if (faces.contains("tinos"))
@@ -970,6 +972,9 @@ public class StyleHelper {
             return ResourcesCompat.getFont(context, R.font.fantasy);
 
         if (bundled_fonts) {
+            if (faces.contains("open sans"))
+                return ResourcesCompat.getFont(context, R.font.opensans);
+
             if (faces.contains("arimo") ||
                     faces.contains("arial") ||
                     faces.contains("verdana") ||
@@ -1033,6 +1038,8 @@ public class StyleHelper {
             result.add(new FontDescriptor("cousine", "Cousine (Courier New)", true));
             result.add(new FontDescriptor("lato", "Lato (Calibri)", true));
             result.add(new FontDescriptor("caladea", "Caladea (Cambria)", true));
+            if (BuildConfig.DEBUG)
+                result.add(new FontDescriptor("open sans", "Open Sans", true));
 
             result.add(new FontDescriptor("comic sans", "OpenDyslexic", true));
         }
