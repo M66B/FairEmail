cc59a13917feb6c3d2ba992055bac81009bc4a95
M66B <M66B@users.noreply.github.com>
2022-03-05 09:55:59 +0100

    Use dedicated widget icon (2)

--- app/src/main/java/eu/faircode/email/Widget.java
+++ app/src/main/java/eu/faircode/email/Widget.java
@@ -114,12 +114,12 @@ public class Widget extends AppWidgetProvider {
 
                     if (layout == 1)
                         views.setImageViewResource(R.id.ivMessage, unseen == 0
-                                ? R.drawable.baseline_mail_outline_widget_24
-                                : R.drawable.baseline_mail_widget_24);
+                                ? R.drawable.widget_baseline_mail_outline_widget_24
+                                : R.drawable.widget_baseline_mail_widget_24);
                     else
                         views.setImageViewResource(R.id.ivMessage, unseen == 0
-                                ? R.drawable.twotone_mail_outline_24
-                                : R.drawable.baseline_mail_24);
+                                ? R.drawable.widget_twotone_mail_outline_24
+                                : R.drawable.widget_baseline_mail_24);
                     views.setTextViewText(R.id.tvCount, unseen < 100 ? nf.format(unseen) : "99+");
                     views.setViewVisibility(R.id.tvCount, layout == 1 && unseen == 0 ? View.GONE : View.VISIBLE);
 
