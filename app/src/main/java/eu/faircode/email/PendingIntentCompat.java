a32e118ed35bed649576ebed280aed2278eaa580
M66B <M66B@users.noreply.github.com>
2022-03-31 18:49:00 +0200

    Attempt to workaround Android 11 / Xiaomi pending intent bug

--- app/src/main/java/eu/faircode/email/PendingIntentCompat.java
+++ app/src/main/java/eu/faircode/email/PendingIntentCompat.java
@@ -30,22 +30,24 @@ public class PendingIntentCompat {
     private PendingIntentCompat() {
     }
 
+    // https://developer.android.com/about/versions/12/behavior-changes-12#pending-intent-mutability
+
     public static PendingIntent getActivity(Context context, int requestCode, Intent intent, int flags) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || (flags & PendingIntent.FLAG_MUTABLE) != 0)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S || (flags & PendingIntent.FLAG_MUTABLE) != 0)
             return PendingIntent.getActivity(context, requestCode, intent, flags);
         else
             return PendingIntent.getActivity(context, requestCode, intent, flags | PendingIntent.FLAG_IMMUTABLE);
     }
 
     public static PendingIntent getService(Context context, int requestCode, @NonNull Intent intent, int flags) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || (flags & PendingIntent.FLAG_MUTABLE) != 0)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S || (flags & PendingIntent.FLAG_MUTABLE) != 0)
             return PendingIntent.getService(context, requestCode, intent, flags);
         else
             return PendingIntent.getService(context, requestCode, intent, flags | PendingIntent.FLAG_IMMUTABLE);
     }
 
     static PendingIntent getForegroundService(Context context, int requestCode, @NonNull Intent intent, int flags) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O || (flags & PendingIntent.FLAG_MUTABLE) != 0)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S || (flags & PendingIntent.FLAG_MUTABLE) != 0)
             return PendingIntent.getService(context, requestCode, intent, flags);
         else
             return PendingIntent.getForegroundService(context, requestCode, intent, flags | PendingIntent.FLAG_IMMUTABLE);
