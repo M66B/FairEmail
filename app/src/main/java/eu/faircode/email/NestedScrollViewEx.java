0d70764d1c34c02dd4fd7c8b9d77a99917100520
M66B <M66B@users.noreply.github.com>
2022-04-07 15:00:38 +0200

    Prevent onLayout crash

---
+++ app/src/main/java/eu/faircode/email/NestedScrollViewEx.java
@@ -0,0 +1,31 @@
+package eu.faircode.email;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.widget.NestedScrollView;
+
+public class NestedScrollViewEx extends NestedScrollView {
+    public NestedScrollViewEx(@NonNull Context context) {
+        super(context);
+    }
+
+    public NestedScrollViewEx(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NestedScrollViewEx(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        try {
+            super.onLayout(changed, l, t, r, b);
+        } catch (Throwable ex) {
+            Log.w(ex);
+        }
+    }
+}
--- app/src/main/java/eu/faircode/email/ScrollViewEx.java
+++ app/src/main/java/eu/faircode/email/ScrollViewEx.java
@@ -39,4 +39,73 @@ public class ScrollViewEx extends ScrollView {
     public ScrollViewEx(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        try {
+            super.onLayout(changed, l, t, r, b);
+        } catch (Throwable ex) {
+            Log.w(ex);
+            /*
+                java.lang.IndexOutOfBoundsException: Index: 1, Size: 1
+                    at java.util.ArrayList.get(ArrayList.java:437)
+                    at android.widget.ArrayAdapter.getItem(ArrayAdapter.java:394)
+                    at android.widget.ArrayAdapter.createViewFromResource(ArrayAdapter.java:450)
+                    at android.widget.ArrayAdapter.getView(ArrayAdapter.java:416)
+                    at android.widget.Spinner.makeView(Spinner.java:724)
+                    at android.widget.Spinner.layout(Spinner.java:672)
+                    at android.widget.Spinner.onLayout(Spinner.java:634)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at androidx.constraintlayout.widget.ConstraintLayout.onLayout(SourceFile:12)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at android.widget.FrameLayout.layoutChildren(FrameLayout.java:332)
+                    at android.widget.FrameLayout.onLayout(FrameLayout.java:270)
+                    at android.widget.ScrollView.onLayout(ScrollView.java:1701)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at android.widget.FrameLayout.layoutChildren(FrameLayout.java:332)
+                    at android.widget.FrameLayout.onLayout(FrameLayout.java:270)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at androidx.drawerlayout.widget.DrawerLayout.onLayout(SourceFile:10)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at android.widget.FrameLayout.layoutChildren(FrameLayout.java:332)
+                    at android.widget.FrameLayout.onLayout(FrameLayout.java:270)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at androidx.appcompat.widget.ActionBarOverlayLayout.onLayout(SourceFile:11)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at android.widget.FrameLayout.layoutChildren(FrameLayout.java:332)
+                    at android.widget.FrameLayout.onLayout(FrameLayout.java:270)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at android.widget.LinearLayout.setChildFrame(LinearLayout.java:1841)
+                    at android.widget.LinearLayout.layoutVertical(LinearLayout.java:1673)
+                    at android.widget.LinearLayout.onLayout(LinearLayout.java:1582)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at android.widget.FrameLayout.layoutChildren(FrameLayout.java:332)
+                    at android.widget.FrameLayout.onLayout(FrameLayout.java:270)
+                    at com.android.internal.policy.DecorView.onLayout(DecorView.java:889)
+                    at android.view.View.layout(View.java:23347)
+                    at android.view.ViewGroup.layout(ViewGroup.java:6563)
+                    at android.view.ViewRootImpl.performLayout(ViewRootImpl.java:4029)
+                    at android.view.ViewRootImpl.performTraversals(ViewRootImpl.java:3434)
+                    at android.view.ViewRootImpl.doTraversal(ViewRootImpl.java:2382)
+                    at android.view.ViewRootImpl$TraversalRunnable.run(ViewRootImpl.java:9088)
+                    at android.view.Choreographer$CallbackRecord.run(Choreographer.java:1521)
+                    at android.view.Choreographer.doCallbacks(Choreographer.java:1319)
+                    at android.view.Choreographer.doFrame(Choreographer.java:1020)
+                    at android.view.Choreographer$FrameDisplayEventReceiver.run(Choreographer.java:1506)
+                    at android.os.Handler.handleCallback(Handler.java:938)
+                    at android.os.Handler.dispatchMessage(Handler.java:99)
+                    at android.os.Looper.loop(Looper.java:262)
+                    at android.app.ActivityThread.main(ActivityThread.java:8304)
+             */
+        }
+    }
 }
