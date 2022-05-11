1a8aa95ec8691cfc4396686ee4eb3aee80a83075
M66B <M66B@users.noreply.github.com>
2022-05-11 08:40:16 +0200

    Small improvement

--- app/src/main/java/eu/faircode/email/Core.java
+++ app/src/main/java/eu/faircode/email/Core.java
@@ -2530,6 +2530,7 @@ class Core {
                             parent = db.folder().getFolderByName(account.id, fullName.substring(0, sep));
 
                         if (!EntityFolder.USER.equals(type) && !EntityFolder.SYSTEM.equals(type)) {
+                            prefs.edit().remove("updated." + account.id + "." + folder.type).apply();
                             EntityFolder has = db.folder().getFolderByType(account.id, type);
                             if (has != null)
                                 type = EntityFolder.USER;
