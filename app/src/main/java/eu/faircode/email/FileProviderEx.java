25babe4f41b2d3b5ef8e1d6323bb94f9141bcf33
M66B <M66B@users.noreply.github.com>
2022-05-11 20:03:58 +0200

    Updated AndroidX

---
+++ app/src/main/java/eu/faircode/email/FileProviderEx.java
@@ -0,0 +1,29 @@
+package eu.faircode.email;
+
+/*
+    This file is part of FairEmail.
+
+    FairEmail is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FairEmail is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FairEmail.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2018-2022 by Marcel Bokhorst (M66B)
+*/
+
+import androidx.core.content.FileProvider;
+
+public class FileProviderEx extends FileProvider {
+    // https://android-review.googlesource.com/c/platform/frameworks/support/+/1978527
+    public FileProviderEx() {
+        super(R.xml.fileprovider_paths);
+    }
+}
