457ecbff6d31db289c3faadcd45bb63f3447cb1b
M66B <M66B@users.noreply.github.com>
2022-07-14 19:30:51 +0200

    Added comment

--- app/src/main/java/eu/faircode/email/ActivityBase.java
+++ app/src/main/java/eu/faircode/email/ActivityBase.java
@@ -783,6 +783,7 @@ abstract class ActivityBase extends AppCompatActivity implements SharedPreferenc
 
     public void performBack() {
         if (getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
+            // https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/app/Activity.java#3896
             ActionBar ab = getSupportActionBar();
             if (ab != null && ab.collapseActionView())
                 return;
@@ -792,7 +793,6 @@ abstract class ActivityBase extends AppCompatActivity implements SharedPreferenc
         }
         finish();
     }
-
 
     public void onBackPressedFragment() {
         performBack();
