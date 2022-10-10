9be1a2e6341332283d47ba5191fbd408d255cc0f
M66B <M66B@users.noreply.github.com>
2022-10-10 19:32:19 +0200

    Changed default folder parameters for new accounts

--- app/src/main/java/eu/faircode/email/EntityFolder.java
+++ app/src/main/java/eu/faircode/email/EntityFolder.java
@@ -237,7 +237,7 @@ public class EntityFolder extends EntityOrder implements Serializable {
     }};
 
     static final int DEFAULT_SYNC = 7; // days
-    static final int DEFAULT_KEEP = 30; // days
+    static final int DEFAULT_KEEP = 60; // days
     static final int DEFAULT_KEEP_DRAFTS = 180; // days
 
     private static final List<String> SYSTEM_FOLDER_SYNC = Collections.unmodifiableList(Arrays.asList(
@@ -259,8 +259,8 @@ public class EntityFolder extends EntityOrder implements Serializable {
     private static final List<Boolean> SYSTEM_FOLDER_DOWNLOAD = Collections.unmodifiableList(Arrays.asList(
             true, // inbox
             true, // drafts
-            false, // sent
-            false, // archive
+            true, // sent
+            true, // archive
             false, // trash
             false // junk
     )); // MUST match SYSTEM_FOLDER_SYNC
