94f04dcaf5765f35e5ff5ffe52a8d04c79951d52
M66B <M66B@users.noreply.github.com>
2022-10-18 12:34:13 +0200

    Disable partial fetch for Yahoo

--- app/src/main/java/eu/faircode/email/DB.java
+++ app/src/main/java/eu/faircode/email/DB.java
@@ -68,7 +68,7 @@ import javax.mail.internet.InternetAddress;
 // https://developer.android.com/topic/libraries/architecture/room.html
 
 @Database(
-        version = 249,
+        version = 250,
         entities = {
                 EntityIdentity.class,
                 EntityAccount.class,
@@ -2499,6 +2499,11 @@ public abstract class DB extends RoomDatabase {
                         Fts4DbHelper.delete(context);
                         Fts5DbHelper.delete(context);
                         db.execSQL("UPDATE `message` SET fts = 0");
+                    }
+                }).addMigrations(new Migration(249, 250) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase db) {
+                        db.execSQL("UPDATE `account` SET partial_fetch = 0 WHERE host = 'imap.mail.yahoo.com'");
                     }
                 }).addMigrations(new Migration(998, 999) {
                     @Override
