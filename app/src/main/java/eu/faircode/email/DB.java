256ea4ac2740e224c6a39768c96b1b5c750ae511
M66B <M66B@users.noreply.github.com>
2023-02-02 21:55:43 +0100

    Needed for a workaround

--- app/src/main/java/eu/faircode/email/DB.java
+++ app/src/main/java/eu/faircode/email/DB.java
@@ -68,7 +68,7 @@ import javax.mail.internet.InternetAddress;
 // https://developer.android.com/topic/libraries/architecture/room.html
 
 @Database(
-        version = 266,
+        version = 267,
         entities = {
                 EntityIdentity.class,
                 EntityAccount.class,
@@ -2728,6 +2728,12 @@ public abstract class DB extends RoomDatabase {
                         db.execSQL("CREATE INDEX `index_message_replying` ON `message` (`replying`)");
                         db.execSQL("CREATE INDEX `index_message_forwarding` ON `message` (`forwarding`)");
                         createTriggers(db);
+                    }
+                }).addMigrations(new Migration(266, 267) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase db) {
+                        logMigration(startVersion, endVersion);
+                        // Do nothing
                     }
                 }).addMigrations(new Migration(998, 999) {
                     @Override
