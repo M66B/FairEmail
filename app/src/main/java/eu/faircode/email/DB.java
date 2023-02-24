3ee444242a1db688b7238cf739772474772251d2
M66B <M66B@users.noreply.github.com>
2023-02-24 21:07:57 +0100

    Enabled raw fetch for Yahoo

--- app/src/main/java/eu/faircode/email/DB.java
+++ app/src/main/java/eu/faircode/email/DB.java
@@ -68,7 +68,7 @@ import javax.mail.internet.InternetAddress;
 // https://developer.android.com/topic/libraries/architecture/room.html
 
 @Database(
-        version = 270,
+        version = 271,
         entities = {
                 EntityIdentity.class,
                 EntityAccount.class,
@@ -2752,6 +2752,13 @@ public abstract class DB extends RoomDatabase {
                     public void migrate(@NonNull SupportSQLiteDatabase db) {
                         logMigration(startVersion, endVersion);
                         db.execSQL("UPDATE account SET partial_fetch = 1" +
+                                " WHERE host = 'imap.mail.yahoo.com' OR host = 'imap.aol.com'");
+                    }
+                }).addMigrations(new Migration(270, 271) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase db) {
+                        logMigration(startVersion, endVersion);
+                        db.execSQL("UPDATE account SET partial_fetch = 0, raw_fetch = 1" +
                                 " WHERE host = 'imap.mail.yahoo.com' OR host = 'imap.aol.com'");
                     }
                 }).addMigrations(new Migration(998, 999) {
