687c168cea762d08bb6a14fadacb8367eaec4f7c
M66B <M66B@users.noreply.github.com>
2023-03-03 22:53:49 +0100

    Yahoo fixed the character encoding problems

--- app/src/main/java/eu/faircode/email/DB.java
+++ app/src/main/java/eu/faircode/email/DB.java
@@ -68,7 +68,7 @@ import javax.mail.internet.InternetAddress;
 // https://developer.android.com/topic/libraries/architecture/room.html
 
 @Database(
-        version = 271,
+        version = 272,
         entities = {
                 EntityIdentity.class,
                 EntityAccount.class,
@@ -2759,6 +2759,13 @@ public abstract class DB extends RoomDatabase {
                     public void migrate(@NonNull SupportSQLiteDatabase db) {
                         logMigration(startVersion, endVersion);
                         db.execSQL("UPDATE account SET partial_fetch = 0, raw_fetch = 1" +
+                                " WHERE host = 'imap.mail.yahoo.com' OR host = 'imap.aol.com'");
+                    }
+                }).addMigrations(new Migration(271, 272) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase db) {
+                        logMigration(startVersion, endVersion);
+                        db.execSQL("UPDATE account SET partial_fetch = 1, raw_fetch = 0" +
                                 " WHERE host = 'imap.mail.yahoo.com' OR host = 'imap.aol.com'");
                     }
                 }).addMigrations(new Migration(998, 999) {
