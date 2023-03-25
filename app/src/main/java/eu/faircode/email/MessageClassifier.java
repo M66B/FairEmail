0d883b50c8aa4b85d730fab708cf91c5bbf125a3
M66B <M66B@users.noreply.github.com>
2023-03-25 14:44:38 +0100

    Skip classifying hidden messages

--- app/src/main/java/eu/faircode/email/MessageClassifier.java
+++ app/src/main/java/eu/faircode/email/MessageClassifier.java
@@ -67,6 +67,9 @@ public class MessageClassifier {
             if (!folder.auto_classify_source)
                 return;
 
+            if (message.ui_hide)
+                return;
+
             long start = new Date().getTime();
 
             // Build text to classify
