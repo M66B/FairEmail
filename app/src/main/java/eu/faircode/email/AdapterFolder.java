1835565ea32555128fa5c45c4d2267315de27fe0
M66B <M66B@users.noreply.github.com>
2023-05-11 12:00:44 +0200

    Added batch add to/delete from unified inbox

--- app/src/main/java/eu/faircode/email/AdapterFolder.java
+++ app/src/main/java/eu/faircode/email/AdapterFolder.java
@@ -675,6 +675,8 @@ public class AdapterFolder extends RecyclerView.Adapter<AdapterFolder.ViewHolder
                 submenu.add(Menu.FIRST, R.string.title_synchronize_batch_disable, 3, R.string.title_synchronize_batch_disable);
                 submenu.add(Menu.FIRST, R.string.title_notify_batch_enable, 4, R.string.title_notify_batch_enable);
                 submenu.add(Menu.FIRST, R.string.title_notify_batch_disable, 5, R.string.title_notify_batch_disable);
+                submenu.add(Menu.FIRST, R.string.title_unified_inbox_add, 6, R.string.title_unified_inbox_add);
+                submenu.add(Menu.FIRST, R.string.title_unified_inbox_delete, 7, R.string.title_unified_inbox_delete);
                 submenu.add(Menu.FIRST, R.string.title_navigation_folder, 6, R.string.title_navigation_folder);
                 submenu.add(Menu.FIRST, R.string.title_navigation_folder_hide, 7, R.string.title_navigation_folder_hide);
                 submenu.add(Menu.FIRST, R.string.title_synchronize_more, 8, R.string.title_synchronize_more);
@@ -709,6 +711,12 @@ public class AdapterFolder extends RecyclerView.Adapter<AdapterFolder.ViewHolder
                             return true;
                         } else if (itemId == R.string.title_notify_batch_disable) {
                             onActionEnableNotify(false);
+                            return true;
+                        } else if (itemId == R.string.title_unified_inbox_add) {
+                            onActionUnifiedInbox(true);
+                            return true;
+                        } else if (itemId == R.string.title_unified_inbox_delete) {
+                            onActionUnifiedInbox(false);
                             return true;
                         } else if (itemId == R.string.title_navigation_folder) {
                             onActionEnableNavigationMenu(true);
@@ -943,6 +951,42 @@ public class AdapterFolder extends RecyclerView.Adapter<AdapterFolder.ViewHolder
                             Log.unexpectedError(parentFragment.getParentFragmentManager(), ex);
                         }
                     }.execute(context, owner, args, "enable");
+                }
+
+                private void onActionUnifiedInbox(boolean add) {
+                    Bundle args = new Bundle();
+                    args.putLong("id", folder.id);
+                    args.putBoolean("add", add);
+
+                    new SimpleTask<Void>() {
+                        @Override
+                        protected Void onExecute(Context context, Bundle args) throws Throwable {
+                            long id = args.getLong("id");
+                            boolean add = args.getBoolean("add");
+
+                            DB db = DB.getInstance(context);
+                            try {
+                                db.beginTransaction();
+                                List<EntityFolder> childs = db.folder().getChildFolders(id);
+                                if (childs == null)
+                                    return null;
+
+                                for (EntityFolder child : childs)
+                                    db.folder().setFolderUnified(child.id, add);
+
+                                db.setTransactionSuccessful();
+                            } finally {
+                                db.endTransaction();
+                            }
+
+                            return null;
+                        }
+
+                        @Override
+                        protected void onException(Bundle args, Throwable ex) {
+                            Log.unexpectedError(parentFragment.getParentFragmentManager(), ex);
+                        }
+                    }.execute(context, owner, args, "unified");
                 }
 
                 private void onActionEnableNavigationMenu(boolean enabled) {
