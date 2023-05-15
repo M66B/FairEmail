380e4cb0e0d9e05e0fc88c1b0e09f4dd62e1ef7f
M66B <M66B@users.noreply.github.com>
2023-05-15 17:14:42 +0200

    Fixed ISO-2022-JP decoding

--- app/src/main/java/eu/faircode/email/MessageHelper.java
+++ app/src/main/java/eu/faircode/email/MessageHelper.java
@@ -3339,7 +3339,7 @@ public class MessageHelper {
             MimeTextPart p1 = parts.get(p);
             MimeTextPart p2 = parts.get(p + 1);
             // https://bugzilla.mozilla.org/show_bug.cgi?id=1374149
-            if (!"__ISO-2022-JP__".equalsIgnoreCase(p1.charset) &&
+            if (!"ISO-2022-JP".equalsIgnoreCase(p1.charset) &&
                     p1.charset != null && p1.charset.equalsIgnoreCase(p2.charset) &&
                     p1.encoding != null && p1.encoding.equalsIgnoreCase(p2.encoding) &&
                     p1.text != null && !p1.text.endsWith("=")) {
