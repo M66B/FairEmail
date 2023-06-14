bcffeaa90dbd2554896d30750a72e710a76d8413
M66B <M66B@users.noreply.github.com>
2023-06-14 20:25:30 +0200

    Who?

--- app/src/main/java/eu/faircode/email/FragmentMessages.java
+++ app/src/main/java/eu/faircode/email/FragmentMessages.java
@@ -6895,7 +6895,7 @@ public class FragmentMessages extends FragmentBase
         if (BuildConfig.DEBUG)
             updateDebugInfo();
 
-        Log.i("List state who=" + Helper.getWho(this) + "" +
+        Log.i("List state" +
                 " reason=" + reason +
                 " tasks=" + tasks + " loading=" + loading +
                 " items=" + items + " initialized=" + initialized +
