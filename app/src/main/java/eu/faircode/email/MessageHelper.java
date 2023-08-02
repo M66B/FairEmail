1d6ab54435e275f43ae879dfba03ab5585ee32ad
M66B <M66B@users.noreply.github.com>
2023-08-02 17:23:23 +0200

    Display inline HTML parts

--- app/src/main/java/eu/faircode/email/MessageHelper.java
+++ app/src/main/java/eu/faircode/email/MessageHelper.java
@@ -4973,7 +4973,7 @@ public class MessageHelper {
 
             String ct = contentType.getBaseType();
             if (("text/plain".equalsIgnoreCase(ct) || "text/html".equalsIgnoreCase(ct)) &&
-                    !Part.ATTACHMENT.equalsIgnoreCase(disposition) && TextUtils.isEmpty(filename) &&
+                    !Part.ATTACHMENT.equalsIgnoreCase(disposition) &&
                     (size <= MAX_MESSAGE_SIZE || size == Integer.MAX_VALUE)) {
                 parts.text.add(new PartHolder(part, contentType));
             } else {
