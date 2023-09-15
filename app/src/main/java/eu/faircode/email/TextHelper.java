b4808153631e26fd580f5a98f56c2ba73fc1339f
M66B <M66B@users.noreply.github.com>
2023-09-15 19:58:56 +0200

    Reduced conversation actions timeout

--- app/src/main/java/eu/faircode/email/TextHelper.java
+++ app/src/main/java/eu/faircode/email/TextHelper.java
@@ -65,7 +65,7 @@ public class TextHelper {
     private static final float MIN_DETECT_PROBABILITY = 0.80f;
     private static final String TRANSLITERATOR = "Any-Latin; Latin-ASCII";
     private static final int MAX_CONVERSATION_SAMPLE_SIZE = 8192;
-    private static final long MAX_CONVERSATION_DURATION = 3000; // milliseconds
+    private static final long MAX_CONVERSATION_DURATION = 2500; // milliseconds
 
     private static final ExecutorService executor =
             Helper.getBackgroundExecutor(1, "text");
