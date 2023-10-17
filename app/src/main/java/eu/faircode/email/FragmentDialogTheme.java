1f8aa6ce93548a6be1c1c9f90954f5921e647fca
M66B <M66B@users.noreply.github.com>
2023-10-17 11:26:56 +0200

    Using You background color

--- app/src/main/java/eu/faircode/email/FragmentDialogTheme.java
+++ app/src/main/java/eu/faircode/email/FragmentDialogTheme.java
@@ -627,16 +627,23 @@ public class FragmentDialogTheme extends FragmentDialogBase {
         String theme = prefs.getString("theme", "blue_orange_system");
         boolean dark = Helper.isDarkTheme(context);
         boolean solarized = (theme != null && theme.startsWith("solarized"));
+        boolean you = (theme != null && theme.startsWith("you_"));
 
         if (cards) {
-            if (compose) {
-                if (!dark || solarized)
-                    view.setBackgroundColor(Helper.resolveColor(context, R.attr.colorCardBackground));
-            } else {
-                if (!dark && !solarized)
-                    view.setBackgroundColor(ContextCompat.getColor(context, beige
-                            ? R.color.lightColorBackground_cards_beige
-                            : R.color.lightColorBackground_cards));
+            if (you && Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+                view.setBackgroundColor(ContextCompat.getColor(context, dark
+                        ? android.R.color.system_background_dark
+                        : android.R.color.system_background_light));
+            else {
+                if (compose) {
+                    if (!dark || solarized)
+                        view.setBackgroundColor(Helper.resolveColor(context, R.attr.colorCardBackground));
+                } else {
+                    if (!dark && !solarized)
+                        view.setBackgroundColor(ContextCompat.getColor(context, beige
+                                ? R.color.lightColorBackground_cards_beige
+                                : R.color.lightColorBackground_cards));
+                }
             }
         } else {
             if (tabular_card_bg)
