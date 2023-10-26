a35d7a084e2eb852eff691a2b8345470baf54526
M66B <M66B@users.noreply.github.com>
2023-10-26 12:52:59 +0200

    Revert "Process event updates from other apps (2)"
    This reverts commit c19b5e1ddbfb2816c0048c098ba5a859bf9b1607.

--- app/src/main/java/eu/faircode/email/MessageHelper.java
+++ app/src/main/java/eu/faircode/email/MessageHelper.java
@@ -4446,7 +4446,9 @@ public class MessageHelper {
                 // https://www.rfc-editor.org/rfc/rfc5546#section-3.2
                 if (method != null && method.isCancel())
                     CalendarHelper.delete(context, event, message);
-                else if (method == null || method.isRequest() || method.isReply()) {
+                else if (method != null && method.isReply())
+                    CalendarHelper.update(context, event, message);
+                else if (method == null || method.isRequest()) {
                     String selectedAccount;
                     String selectedName;
                     try {
