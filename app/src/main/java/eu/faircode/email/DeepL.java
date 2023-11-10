5ded10a29a6fedba8276f9655cdac1ff126f7cbf
M66B <M66B@users.noreply.github.com>
2023-11-10 09:30:11 +0100

    DeepL: fixed NPE

--- app/src/main/java/eu/faircode/email/DeepL.java
+++ app/src/main/java/eu/faircode/email/DeepL.java
@@ -347,7 +347,7 @@ public class DeepL {
     }
 
     private static String getBaseUri(String key) {
-        String domain = (key.endsWith(":fx") ? "api-free.deepl.com" : "api.deepl.com");
+        String domain = (key != null && key.endsWith(":fx") ? "api-free.deepl.com" : "api.deepl.com");
         return "https://" + domain + "/v2/";
     }
 
