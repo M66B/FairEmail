aaafe78bef37d4f159c3b13efd99c0be1fd03e91
M66B <M66B@users.noreply.github.com>
2023-11-20 10:06:04 +0100

    ROOM temp dir

--- app/src/main/java/eu/faircode/email/Helper.java
+++ app/src/main/java/eu/faircode/email/Helper.java
@@ -2633,7 +2633,7 @@ public class Helper {
 
     private static final Map<File, Boolean> exists = new HashMap<>();
 
-    static File ensureExists(File dir) {
+    public static File ensureExists(File dir) {
         synchronized (exists) {
             if (exists.containsKey(dir))
                 return dir;
