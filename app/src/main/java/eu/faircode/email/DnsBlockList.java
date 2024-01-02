68792a985967273e9605ee70aae9d8d7fa045b40
M66B <M66B@users.noreply.github.com>
2024-01-02 16:14:21 +0100

    Refactoring

--- app/src/main/java/eu/faircode/email/DnsBlockList.java
+++ app/src/main/java/eu/faircode/email/DnsBlockList.java
@@ -205,7 +205,7 @@ public class DnsBlockList {
         for (BlockList blocklist : blocklists)
             if (isEnabled(context, blocklist) &&
                     blocklist.numeric == numeric &&
-                    isJunk(host, blocklist)) {
+                    isJunk(context, host, blocklist)) {
                 blocked = true;
                 break;
             }
@@ -217,11 +217,11 @@ public class DnsBlockList {
         return blocked;
     }
 
-    private static boolean isJunk(String host, BlockList blocklist) {
+    private static boolean isJunk(Context context, String host, BlockList blocklist) {
         try {
             if (blocklist.numeric) {
                 long start = new Date().getTime();
-                InetAddress[] addresses = InetAddress.getAllByName(host);
+                InetAddress[] addresses = DnsHelper.getAllByName(context, host);
                 long elapsed = new Date().getTime() - start;
                 Log.i("isJunk resolved=" + host + " elapse=" + elapsed + " ms");
                 for (InetAddress addr : addresses) {
@@ -249,7 +249,7 @@ public class DnsBlockList {
 
                         lookup.append(blocklist.address);
 
-                        if (isJunk(lookup.toString(), blocklist.responses))
+                        if (isJunk(context, lookup.toString(), blocklist.responses))
                             return true;
                     } catch (Throwable ex) {
                         Log.w(ex);
@@ -258,7 +258,7 @@ public class DnsBlockList {
             } else {
                 long start = new Date().getTime();
                 String lookup = host + "." + blocklist.address;
-                boolean junk = isJunk(lookup, blocklist.responses);
+                boolean junk = isJunk(context, lookup, blocklist.responses);
                 long elapsed = new Date().getTime() - start;
                 Log.i("isJunk" + " " + lookup + "=" + junk + " elapsed=" + elapsed);
                 return junk;
@@ -270,12 +270,12 @@ public class DnsBlockList {
         return false;
     }
 
-    private static boolean isJunk(String lookup, InetAddress[] responses) {
+    private static boolean isJunk(Context context, String lookup, InetAddress[] responses) {
         long start = new Date().getTime();
         InetAddress result;
         try {
             // Possibly blocked
-            result = InetAddress.getByName(lookup);
+            result = DnsHelper.getByName(context, lookup);
         } catch (UnknownHostException ignored) {
             // Not blocked
             result = null;
