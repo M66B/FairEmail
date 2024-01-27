2bbeba77edd56940cea0b80f0dc99bb5e8bd0b75
M66B <M66B@users.noreply.github.com>
2024-01-27 07:50:22 +0100

    Revert "Refactoring"
    This reverts commit 004b37d16cbacef966618cf4684bba2193d5bf24.

--- app/src/main/java/eu/faircode/email/AdapterMedia.java
+++ app/src/main/java/eu/faircode/email/AdapterMedia.java
@@ -282,7 +282,7 @@ public class AdapterMedia extends RecyclerView.Adapter<AdapterMedia.ViewHolder> 
                         if (MediaPlayerHelper.isPlaying(uri))
                             MediaPlayerHelper.stopMusic(context);
                         else
-                            MediaPlayerHelper.startMusic(context, owner, uri,
+                            MediaPlayerHelper.startMusic(context, uri,
                                     new RunnableEx("player") {
                                         @Override
                                         public void delegate() {
@@ -390,6 +390,7 @@ public class AdapterMedia extends RecyclerView.Adapter<AdapterMedia.ViewHolder> 
             @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
             public void onDestroyed() {
                 Log.d(AdapterMedia.this + " parent destroyed");
+                MediaPlayerHelper.stopMusic(context);
                 AdapterMedia.this.parentFragment = null;
                 owner.getLifecycle().removeObserver(this);
             }
