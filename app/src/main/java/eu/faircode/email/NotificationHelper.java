deafcb1abb2c8e339fc53c3ef3f6b82924ea119a
M66B <M66B@users.noreply.github.com>
2024-02-02 09:34:44 +0100

    Respect system DND setting

--- app/src/main/java/eu/faircode/email/NotificationHelper.java
+++ app/src/main/java/eu/faircode/email/NotificationHelper.java
@@ -138,7 +138,7 @@ class NotificationHelper {
         notification.enableLights(true);
         notification.setLightColor(Color.YELLOW);
         notification.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-        notification.setBypassDnd(true);
+        //notification.setBypassDnd(true);
         createNotificationChannel(nm, notification);
 
         NotificationChannel progress = new NotificationChannel(
