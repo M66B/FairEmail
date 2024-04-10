8dbf37884f96bb70f12f31c9770d6f2de291475d
M66B <M66B@users.noreply.github.com>
2024-04-10 07:36:30 +0200

    Require inline for inline text

--- app/src/main/java/eu/faircode/email/MessageHelper.java
+++ app/src/main/java/eu/faircode/email/MessageHelper.java
@@ -5119,7 +5119,7 @@ public class MessageHelper {
 
             String ct = contentType.getBaseType();
             if (("text/plain".equalsIgnoreCase(ct) || "text/html".equalsIgnoreCase(ct)) &&
-                    !Part.ATTACHMENT.equalsIgnoreCase(disposition) &&
+                    Part.INLINE.equalsIgnoreCase(disposition) &&
                     (size <= MAX_MESSAGE_SIZE || size == Integer.MAX_VALUE)) {
                 parts.text.add(new PartHolder(part, contentType));
             } else {
