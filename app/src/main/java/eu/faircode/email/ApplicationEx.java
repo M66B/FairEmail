e39608cb5fb362b79645b5494e31c8d007a7052e
M66B <M66B@users.noreply.github.com>
2024-05-29 12:04:17 +0200

    Enable changelog once

--- app/src/main/java/eu/faircode/email/ApplicationEx.java
+++ app/src/main/java/eu/faircode/email/ApplicationEx.java
@@ -868,7 +868,8 @@ public class ApplicationEx extends Application
                 editor.putBoolean("delete_unseen", false);
             if (Helper.isPixelBeta())
                 editor.putBoolean("motd", true);
-        }
+        } else if (version < 2191)
+            editor.remove("show_changelog");
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !BuildConfig.DEBUG)
             editor.remove("background_service");
