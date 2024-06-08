74541393ab36a6a6c61f9f77623e38a86c159a33
M66B <M66B@users.noreply.github.com>
2024-06-08 16:07:53 +0200

    Fixed hyperlinks starting/ending with spaces

--- app/src/main/java/eu/faircode/email/AdapterMessage.java
+++ app/src/main/java/eu/faircode/email/AdapterMessage.java
@@ -6545,9 +6545,9 @@ public class AdapterMessage extends RecyclerView.Adapter<AdapterMessage.ViewHold
             Log.i("Opening uri=" + uri + " title=" + title + " always confirm=" + always_confirm);
 
             try {
-                uri = Uri.parse(uri.toString().replaceAll("[\r\n]", ""));
+                uri = Uri.parse(uri.toString().trim().replaceAll("[\r\n]", ""));
                 if (UriHelper.isHyperLink(uri))
-                    uri = Uri.parse(uri.toString().trim().replaceAll("\\s+", "+"));
+                    uri = Uri.parse(uri.toString().replaceAll("\\s+", "+"));
 
                 if (ProtectedContent.isProtectedContent(uri)) {
                     Bundle args = new Bundle();
