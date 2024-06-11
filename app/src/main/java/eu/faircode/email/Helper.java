2798a6a6c812546ebcbfa9b057c6cac4cf599afb
M66B <M66B@users.noreply.github.com>
2024-06-11 22:43:59 +0200

    Revert "Immediately hide keyboard"
    This reverts commit 13396cb100262b6348148a5186e073726a5a54af.

--- app/src/main/java/eu/faircode/email/Helper.java
+++ app/src/main/java/eu/faircode/email/Helper.java
@@ -2111,12 +2111,13 @@ public class Helper {
     }
 
     static void hideKeyboard(final View view) {
-        try {
-            Log.i("hideKeyboard view=" + view);
-            new SoftwareKeyboardControllerCompat(view).hide();
-        } catch (Throwable ex) {
-            Log.e(ex);
-        }
+        view.post(new RunnableEx("hideKeyboard") {
+            @Override
+            protected void delegate() {
+                Log.i("hideKeyboard view=" + view);
+                new SoftwareKeyboardControllerCompat(view).hide();
+            }
+        });
     }
 
     static boolean isKeyboardVisible(final View view) {
