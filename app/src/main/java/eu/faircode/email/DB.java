5e31abb7b6a1c8ab37394efb8729f387dae830b1
M66B <M66B@users.noreply.github.com>
2024-07-04 07:24:46 +0200

    Enable no-op for iCloud accounts

--- app/src/main/java/eu/faircode/email/DB.java
+++ app/src/main/java/eu/faircode/email/DB.java
@@ -70,7 +70,7 @@ import javax.mail.internet.InternetAddress;
 // https://developer.android.com/topic/libraries/architecture/room.html
 
 @Database(
-        version = 298,
+        version = 299,
         entities = {
                 EntityIdentity.class,
                 EntityAccount.class,
@@ -3031,6 +3031,13 @@ public abstract class DB extends RoomDatabase {
                         logMigration(startVersion, endVersion);
                         db.execSQL("DROP VIEW IF EXISTS `account_view`");
                         db.execSQL("CREATE VIEW IF NOT EXISTS `account_view` AS " + TupleAccountView.query);
+                    }
+                }).addMigrations(new Migration(298, 299) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase db) {
+                        logMigration(startVersion, endVersion);
+                        db.execSQL("UPDATE account SET keep_alive_noop = 1" +
+                                " WHERE host = 'imap.mail.me.com' AND pop = " + EntityAccount.TYPE_IMAP);
                     }
                 }).addMigrations(new Migration(998, 999) {
                     @Override
