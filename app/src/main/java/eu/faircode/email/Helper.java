ff1232c48a00ed4172d5e0d6e9befd3244a5827f
M66B <M66B@users.noreply.github.com>
2024-11-01 16:44:17 +0100

    RDP is unsafe

--- app/src/main/java/eu/faircode/email/Helper.java
+++ app/src/main/java/eu/faircode/email/Helper.java
@@ -275,6 +275,7 @@ public class Helper {
             "mde", "msc", "msi", "msix", "msixbundle", "msp", "mst",
             "nsh",
             "pif", "ps1",
+            "rdp", // https://www.microsoft.com/en-us/security/blog/2024/10/29/midnight-blizzard-conducts-large-scale-spear-phishing-campaign-using-rdp-files/
             "scr", "sct", "shb", "sys",
             "vb", "vbe", "vbs", "vxd",
             "wsc", "wsf", "wsh"
