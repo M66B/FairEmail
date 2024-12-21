c6ad264b66b61755e2c0ff1c6e87d9a51fb6a143
M66B <M66B@users.noreply.github.com>
2024-12-21 13:07:55 +0100

    Let (semi) colon end sentences

--- app/src/main/java/eu/faircode/email/Helper.java
+++ app/src/main/java/eu/faircode/email/Helper.java
@@ -2793,13 +2793,13 @@ public class Helper {
     }
 
     static boolean isEndChar(char c) {
-        return (isSentenceChar(c) ||
-                c == ',' || c == ':' || c == ';');
+        return (isSentenceChar(c) || c == ',');
     }
 
     static boolean isSentenceChar(char c) {
         return (c == '.' /* Latin */ ||
                 c == '。' /* Chinese */ ||
+                c == ':' || c == ';' ||
                 c == '?' || c == '!');
     }
 
