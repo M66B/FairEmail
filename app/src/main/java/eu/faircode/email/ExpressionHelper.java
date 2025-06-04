c3becf26d63006fe0589e768b24701c69666ac5e
M66B <M66B@users.noreply.github.com>
2025-06-04 15:47:21 +0200

    Cleanup

--- app/src/main/java/eu/faircode/email/ExpressionHelper.java
+++ app/src/main/java/eu/faircode/email/ExpressionHelper.java
@@ -51,7 +51,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.regex.Pattern;
 
 import javax.mail.Address;
