9f830ba4a7411ad9dd7e0972cfd219f4bd16646b
M66B <M66B@users.noreply.github.com>
2025-06-12 07:41:25 +0200

    Revert "Private calendar events"
    This reverts commit 8a1817629c1e98e643fbc8069552902b3cf80600.

--- app/src/main/java/eu/faircode/email/CalendarHelper.java
+++ app/src/main/java/eu/faircode/email/CalendarHelper.java
@@ -264,7 +264,6 @@ public class CalendarHelper {
                     values.put(CalendarContract.Events.DESCRIPTION, description);
                 if (!TextUtils.isEmpty(location))
                     values.put(CalendarContract.Events.EVENT_LOCATION, location);
-                values.put(CalendarContract.Events.ACCESS_LEVEL, CalendarContract.Events.ACCESS_PRIVATE);
                 values.put(CalendarContract.Events.STATUS, status);
 
                 long eventId;
