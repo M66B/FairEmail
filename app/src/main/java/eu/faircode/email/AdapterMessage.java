dc54d462dae8de1e1bbbc8925f17d682a252e5b1
M66B <M66B@users.noreply.github.com>
2026-08-20 12:06:03 +0200

    Show suspicious email addresses in the personal field

--- app/src/main/java/eu/faircode/email/AdapterMessage.java
+++ app/src/main/java/eu/faircode/email/AdapterMessage.java
@@ -5671,8 +5671,19 @@ public class AdapterMessage extends RecyclerView.Adapter<AdapterMessage.ViewHold
                         continue;
                     if (!TextHelper.isSingleScript(personal))
                         return true;
-                    if (false && !BuildConfig.PLAY_STORE_RELEASE && Helper.EMAIL_ADDRESS.matcher(personal).find())
-                        return true;
+                    if (!message.isForwarder()) {
+                        String email = ((InternetAddress) from).getAddress();
+                        Matcher m = Helper.EMAIL_ADDRESS.matcher(personal);
+                        while (m.find())
+                            try {
+                                String e = personal.substring(m.start(), m.end());
+                                if (!e.equalsIgnoreCase(email))
+                                    return true;
+                            } catch (Throwable ex) {
+                                Log.e(ex);
+                                break;
+                            }
+                    }
                 }
 
             return false;
