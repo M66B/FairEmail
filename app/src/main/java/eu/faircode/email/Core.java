2ae118d6cbb7fec37ce0f8e5b10101a0735564e5
M66B <M66B@users.noreply.github.com>
2026-09-04 21:11:29 +0200

    Revert "Added expunge/trash debug option"
    This reverts commit a0dc903efeb489700fbc1f897a64efc0bc671153.

--- app/src/main/java/eu/faircode/email/Core.java
+++ app/src/main/java/eu/faircode/email/Core.java
@@ -1454,9 +1454,6 @@ class Core {
         // Move message
         DB db = DB.getInstance(context);
 
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
-        boolean expunge_trash = pref.getBoolean("expunge_trash", false);
-
         // Get arguments
         long id = jargs.getLong(0);
         boolean seen = jargs.optBoolean(1);
@@ -1621,7 +1618,7 @@ class Core {
                             ex.getCause().getMessage().contains("[EXPUNGEISSUED]")))
                         throw ex;
                 }
-            else if (!expunge_trash || !EntityFolder.TRASH.equals(target.type))
+            else
                 ifolder.copyMessages(map.keySet().toArray(new Message[0]), itarget);
         }
 
