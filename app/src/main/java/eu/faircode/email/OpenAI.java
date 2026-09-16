91f2095ecb32daa130e852a80ac54a2d6bd72e89
M66B <M66B@users.noreply.github.com>
2026-09-16 15:53:50 +0200

    Deprecated max_tokens replaced by max_completion_tokens

--- app/src/main/java/eu/faircode/email/OpenAI.java
+++ app/src/main/java/eu/faircode/email/OpenAI.java
@@ -150,7 +150,7 @@ public class OpenAI {
         JSONObject jquestion = new JSONObject();
         jquestion.put("model", model);
         if (max_tokens > 0)
-            jquestion.put("max_tokens", max_tokens);
+            jquestion.put("max_completion_tokens", max_tokens);
         jquestion.put("messages", jmessages);
         if (temperature != null)
             jquestion.put("temperature", temperature);
