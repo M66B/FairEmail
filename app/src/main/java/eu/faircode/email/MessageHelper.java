2669dfa9361e8b4a370675c5a984e9e1422ec95a
M66B <M66B@users.noreply.github.com>
2026-09-16 16:45:11 +0200

    Prevent double extra replace

--- app/src/main/java/eu/faircode/email/MessageHelper.java
+++ app/src/main/java/eu/faircode/email/MessageHelper.java
@@ -870,11 +870,6 @@ public class MessageHelper {
                 String email = ((InternetAddress) from).getAddress();
                 String user = UriHelper.getEmailUser(email);
                 String domain = UriHelper.getEmailDomain(email);
-                if (message.extra != null) {
-                    Pair<String, String> extra = getExtra(email, message.extra);
-                    if (extra.second != null)
-                        email = extra.second;
-                }
                 address = address
                         .replace("$from$", email)
                         .replace("$user$", user)
